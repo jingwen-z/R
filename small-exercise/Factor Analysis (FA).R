9edef592754bd632b3b94dfb4c260d37cfc17348
jingwen <jingwen7.zheng@gmail.com>
2016-04-09 22:20:09 +0200

    add the second method

--- small-exercise/Factor Analysis (FA).R
+++ small-exercise/Factor Analysis (FA).R
@@ -1,5 +1,6 @@
 ############################## small exercise - FA ################################
 
+### FIRST METHOD ###
 # in fact, we usually use the method "Principle Components Analysis" to estimate loading factors
 # thus, we'll firstly write Factor Analysis function with the method "Principle Components Analysis"
 
@@ -42,7 +43,7 @@ R <- cor(regions.bis)
 options(digits = 3)
 factor.analysis(R,4)
 
-## according to factor.analysis(R,4)$loadings, we can get the following regressions of initial variable and 4 factors
+## according to "factor.analysis(R,4)$loadings", we can get the following regressions of initial variable and 4 factors
 # X1 = 0.9578*F1 - 0.2555*F2 - 0.0444*F3 - 0.0888*F4
 # X2 = 0.7272*F1 + 0.5926*F2 + 0.1493*F3 + 0.3077*F4
 # X3 = -0.0155*F1 - 0.3319*F2 + 0.9420*F3 + 0.0320*F4
@@ -62,8 +63,22 @@ factor.analysis(R,4)
 
 # Factor 4 indicates people's general situation in the region.
 
+### SECOND METHOD ###
+# we can use the funcion "factanal()" directly
 
+factanal(regions.bis,factors = 3)
 
+## According to "factanal(regions.bis,factors = 3)$loadings", we can observe that when the number of factor is 3, the cumulative variance is larger than 0.8.
+## Thus, we could make sure that the number of factor is 3, this result is NOT the same as the one above. 
+## But in my opinion, this method is more reasonale. 
+## Then, we get the following regressions between each factor and variables.
+# F1 = 0.991*popul + 0.442*tact + 0.980*nbentr + 0.907*nbbrev + 0.990*teleph
+# F2 = 0.893*tact + 0.101*nbentr + 0.335*nbbrev - 0.781*chom + 0.132*teleph
+# F3 = 0.875*superf + 0.158*nbentr - 0.193*nbbrev
 
-
-
+## According to the regressions above, we can get the real explanatory meaning as following.
+# Factor 1 is highly correlated to population, number of firms of the region, number of patents taken out during the year and 
+# number of telephone lines in place in the region, thus Factor 1 refelcts the economic development of the regions.
+# Factor 2 is highly correlated to activity rate of the region and unemployment rate, which means Factor 2 indicates 
+# people's general situation in the region.
+# Factor 3 is highly correlated to surface of the region, which reflects Factor 3 indicates the surface of the region.
