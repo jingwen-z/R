8d542ea71cb0a9a7a495337567af23583b209be8
jingwen <jingwen7.zheng@gmail.com>
2016-07-21 00:03:56 +0200

    Update Utilities.R

--- DataCamp/Intermediate-R/Utilities.R
+++ DataCamp/Intermediate-R/Utilities.R
@@ -1,47 +1,145 @@
 ################################### Useful Functions ###################################
-#############  #############
 
+############# Mathematical utilities #############
+# The errors vector has already been defined for you
+errors <- c(1.9, -2.6, 4.0, -9.5, -3.4, 7.3)
 
+# Sum of absolute rounded values of errors
+sum(abs(round(errors)))
 
+############# Find the error #############
+# Don't edit these two lines
+vec1 <- c(1.5, 2.5, 8.4, 3.7, 6.3)
+vec2 <- rev(vec1)
 
-#############  #############
+# Fix the error
+mean(c(abs(vec1), abs(vec2)))
 
+# Fix me: seq(rep(1, 7, by = 2), times = 7)
+rep( seq(from=1, to=7, by=2), times=7 )
 
+############# Data Utilities #############
+# The linkedin and facebook vectors have already been created for you
+linkedin <- list(16, 9, 13, 5, 2, 17, 14)
+facebook <- list(17, 7, 5, 16, 8, 13, 14)
 
+# Convert linkedin and facebook to a vector: li_vec and fb_vec
+li_vec <- as.vector(linkedin)
+fb_vec <- as.vector(facebook)
 
-#############  #############
+# Append fb_vec to li_vec: social_vec
+social_vec <- append(li_vec, fb_vec)
 
+# Sort social_vec
+sort(as.numeric(social_vec), decreasing = TRUE)
 
+############# Beat Gauss using R #############
+# Create first sequence: seq1
+seq1 <- seq(1, 500, by = 3)
 
+# Create second sequence: seq2
+seq2 <- seq(1200, 900, by = -7)
 
-
-
-#############  #############
-
-
-
-
-
+# Calculate total sum of the sequences
+sum(seq1, seq2)
 
 ################################### Regular Expressions ###################################
-#############  #############
 
+############# grepl & grep #############
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org", 
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
 
+# Use grepl() to match for "edu"
+grepl("edu", emails)
 
+# Use grep() to match for "edu", save result to hits
+hits <- grep("edu",emails)
 
+# Subset emails using hits
+emails[hits]
 
+# Use grepl() to match for .edu addresses more robustly
+grepl("@.*\\.edu$", emails )
+# grepl("@.*.edu$", emails )
 
+# Use grep() to match for .edu addresses more robustly, save result to hits
+hits <- grep("@.*\\.edu$", emails)
 
+# Subset emails using hits
+emails[hits]
 
+############# sub & gsub #############
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov", "dalai.lama@peace.org", 
+            "invalid.edu", "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
 
+# Use sub() to convert the email domains to datacamp.edu
+sub("@.*\\.edu$", "@datacamp.edu", emails)
 
+################################### Times and Dates ###################################
 
-#############  #############
+############# Right here, right now #############
+# Get the current date: today
+today <- Sys.Date()
 
+# See what today looks like under the hood
+unclass(today)
 
+# Get the current time: now
+now <- Sys.time()
 
+# See what now looks like under the hood
+unclass(now)
 
+############# Create and format times #############
+# Definition of character strings representing times
+str1 <- "May 23, '96 hours:23 minutes:01 seconds:45"
+str2 <- "2012-3-12 14:23:08"
 
+# Convert the strings to POSIXct objects: time1, time2
+time1 <- as.POSIXct(str1, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")
+time2 <- as.POSIXct(str2)
 
+# Convert times to formatted strings
+format(time1, "%M")
+format(time2, "%I:%M %p")  #!!!!!!!
 
-#############  #############
+############# Calculations with Dates #############
+# day1, day2, day3, day4 and day5 are already available in the workspace
+
+# Difference between last and first pizza day
+day5 - day1
+
+# Create vector pizza
+pizza <- c(day1, day2, day3, day4, day5)
+
+# Create differences between consecutive pizza days: day_diff
+day_diff <- diff(pizza)
+
+# Average period between two consecutive pizza days
+mean(day_diff)
+
+############# Calculations with Times #############
+# login and logout are already defined in the workspace
+# Calculate the difference between login and logout: time_online
+time_online <- logout - login
+
+# Inspect the variable time_online
+time_online
+
+# Calculate the total time online
+sum(time_online)
+
+# Calculate the average time online
+mean(time_online)
+
+############# Time is of the essence #############
+# Convert astro to vector of Date objects: astro_dates
+astro_dates <- as.Date(astro, "%d-%b-%Y")
+
+# Convert meteo to vector of Date objects: meteo_dates
+meteo_dates <- as.Date(meteo, "%B%d, %y")
+
+# Calculate the maximum absolute difference between astro_dates and meteo_dates
+max(abs(astro_dates - meteo_dates))
