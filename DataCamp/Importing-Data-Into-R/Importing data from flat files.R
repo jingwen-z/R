1612611654899ae0f51ffb9bd773c810a1dd087b
jingwen <jingwen7.zheng@gmail.com>
2016-07-23 16:36:36 +0200

    Update Importing data from flat files.R

--- DataCamp/Importing-Data-Into-R/Importing data from flat files.R
+++ DataCamp/Importing-Data-Into-R/Importing data from flat files.R
@@ -65,21 +65,78 @@ str(hotdogs2)
 
 ######################################## readr & data.table ########################################
 
-#############  #############
+############# read_delim #############
+# Just as read.table() was the main utils function, read_delim() is the main readr function.
+# Load the readr package
+library(readr)
 
+# Import potatoes.txt using read_delim(): potatoes
+potatoes <- read_delim("potatoes.txt", delim = "\t")
+# potatoes <- read_tsv("potatoes.txt", col_types = "iiiiiddd")
 
+# Print out potatoes
+potatoes
 
+############# read_csv #############
+# readr is already loaded
 
+# Column names
+properties <- c("area", "temp", "size", "storage", "method", 
+                "texture", "flavor", "moistness")
 
+# Import potatoes.csv with read_csv(): potatoes
+potatoes <- read_csv("potatoes.csv", col_names = properties)
 
+############# col_types, skip and n_max #############
+# readr is already loaded
 
+# Column names
+properties <- c("area", "temp", "size", "storage", "method", 
+                "texture", "flavor", "moistness")
 
-#############  #############
+# Finish the first read_tsv() call to import observations 7, 8, 9, 10 and 11 from potatoes.txt.
+# Import 5 observations from potatoes.txt: potatoes_fragment
+potatoes_fragment <- read_tsv("potatoes.txt", skip = 7, n_max = 5, col_names = properties)
 
+# Import all data, but force all columns to be character: potatoes_char
+potatoes_char <- read_tsv("potatoes.txt", col_types = "cccccccc")
 
+############# col_types with collectors #############
+# readr is already loaded
 
+# Import without col_types
+hotdogs <- read_tsv("hotdogs.txt", col_names = c("type", "calories", "sodium"))
 
+# Display the summary of hotdogs
+summary(hotdogs)
 
+# The collectors you will need to import the data
+fac <- col_factor(levels = c("Beef", "Meat", "Poultry"))
+int <- col_integer()
 
+# Edit the col_types argument: Pass a list() with the elements fac, int and int, so the first column is importead as a factor, and the second and third column as integers.
+# Edit the col_types argument to import the data correctly: hotdogs_factor
+hotdogs_factor <- read_tsv("hotdogs.txt", 
+                           col_names = c("type", "calories", "sodium"),
+                           col_types = list(fac, int, int))
 
-#############  #############
+# Display the summary of hotdogs_factor
+summary(hotdogs_factor)
+
+############# fread #############
+# load the data.table package
+library(data.table)
+
+# Import potatoes.txt with fread(): potatoes
+potatoes <- fread("potatoes.txt")
+
+# Print out potatoes
+potatoes
+
+# fread is already loaded
+
+# Import columns 6 and 8 of potatoes.txt: potatoes
+potatoes <- fread("potatoes.txt", select = c(6, 8))
+
+# Plot texture (x) and moistness (y) of potatoes
+plot(potatoes$texture, potatoes$moistness, xlab = "texture", ylab = "moistness")
