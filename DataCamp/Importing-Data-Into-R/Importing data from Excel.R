2085076667859fdc65df1345e902b0a32b2699d9
jingwen <jingwen7.zheng@gmail.com>
2016-07-25 21:46:54 +0200

    The gdata package

--- DataCamp/Importing-Data-Into-R/Importing data from Excel.R
+++ DataCamp/Importing-Data-Into-R/Importing data from Excel.R
@@ -53,4 +53,44 @@ latitude_sel[1, ]
 
 ######################################## The gdata package ########################################
 
+################# Import a local file #################
+# Load the gdata package 
+library(gdata)
+
+# Import the second sheet of urbanpop.xls: urban_pop
+urban_pop <- read.xls("urbanpop.xls", sheet = "1967-1974")
+
+# Print the first 11 observations using head()
+head(urban_pop, 11)
+
+################# read.xls() wraps around read.table() #################
+# Column names for urban_pop
+columns <- c("country", paste0("year_", 1967:1974))
+
+# Finish the read.xls call: skip the first 50 rows of the sheet.
+urban_pop <- read.xls("urbanpop.xls", sheet = 2, 
+                      skip = 50, header = FALSE, stringsAsFactors = FALSE,
+                      col.names = columns)
+
+# Print first 10 observation of urban_pop
+head(urban_pop, 10)
+
+################# Work that Excel data! #################
+# Add code to import data from all three sheets in urbanpop.xls
+path <- "urbanpop.xls"
+urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
+urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
+urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = FALSE)
+
+# Extend the cbind() call to include urban_sheet3: urban
+urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
+
+# Remove all rows with NAs from urban: urban_clean
+urban_clean <- na.omit(urban)
+
+# Print out a summary of urban_clean
+summary(urban_clean)
+
+######################################## The XLConnect package ########################################
+
 #################  #################
