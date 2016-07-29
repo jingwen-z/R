09526480bf98cf70ff43e04ac89c4ffa0d3dd996
jingwen <jingwen7.zheng@gmail.com>
2016-07-29 23:43:59 +0200

    Update Importing data from other statistical software.R

--- DataCamp/Importing-Data-Into-R/Importing data from other statistical software.R
+++ DataCamp/Importing-Data-Into-R/Importing data from other statistical software.R
@@ -49,4 +49,59 @@ summary(work$GENDER)
 
 ##################################### The foreign package #####################################
 
-####################  ####################
+#################### Import STATA data with foreign ####################
+# Load the foreign package
+library(foreign)
+
+# Import florida.dta and name the resulting data frame florida
+florida <- read.dta("florida.dta")
+
+# Check tail() of florida
+tail(florida, 6)
+
+# Specify the file path using file.path(): path
+path <- file.path("worldbank/edequality.dta")
+
+
+# Create and print structure of edu_equal_1
+# The "edequality.dta" file is located in the "worldbank" folder.
+edu_equal_1 <- read.dta(path)
+str(edu_equal_1)
+
+# Create and print structure of edu_equal_2
+edu_equal_2 <- read.dta(path, convert.factors = FALSE)
+str(edu_equal_2)
+
+# Create and print structure of edu_equal_3
+edu_equal_3 <- read.dta(path, convert.underscore = TRUE)  
+str(edu_equal_3)
+
+#################### Do you know your data? ####################
+# How many observations/individuals from Bulgaria have an income above 1000?
+nrow(subset(edu_equal_1, ethnicity_head == "Bulgaria" & income > 1000)) # 8997
+
+#################### Import SPSS data with foreign ####################
+# foreign is already loaded
+
+# Import international.sav as a data frame: demo
+demo <- read.spss("international.sav", to.data.frame = TRUE)
+
+# Create boxplot of gdp variable of demo
+boxplot(demo$gdp)
+
+# Import international.sav as a data frame, demo_1
+demo_1 <- read.spss("international.sav", to.data.frame = TRUE)
+  
+# Print out the head of demo_1
+head(demo_1)
+
+# Import international.sav as demo_2
+# but this time in a way such that variables with value labels are not converted to R factors
+demo_2 <- read.spss("international.sav", use.value.labels = FALSE, to.data.frame = TRUE)
+
+# Print out the head of demo_2
+head(demo_2)
+
+#################### Excursion: Correlation ####################
+# What is the correlation coefficient for the two numerical variables gdp and f_illit (female illiteracy rate)?
+cor(demo$gdp, demo$f_illit) # -0.4476856
