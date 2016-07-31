33a69903c50ddedfe8b98cc84968f8e3e6fd5c7f
jingwen <jingwen7.zheng@gmail.com>
2016-08-01 00:09:23 +0200

    Importing data from the web - part 1

--- DataCamp/Importing-Data-Into-R/Importing data from the web.R
+++ DataCamp/Importing-Data-Into-R/Importing data from the web.R
@@ -1,1 +1,87 @@
+####################################### Importing data from the web #######################################
 
+################# Import flat files from the web #################
+# Load the readr package
+library(readr)
+
+# Import the csv file: pools
+url_csv <- "http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/swimming_pools.csv"
+pools <- read_csv(url_csv)
+
+# Import the txt file: potatoes
+url_delim <- "http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/potatoes.txt"
+potatoes <- read_tsv(url_delim)
+
+# Print pools and potatoes
+pools
+potatoes
+
+################# Secure importing #################
+# https URL to the swimming_pools csv file.
+url_csv <- "https://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/swimming_pools.csv"
+
+# Import the file using read.csv(): pools1
+pools1 <- read.csv(url_csv)
+
+# Load the readr package
+library(readr)
+
+# Import the file using read_csv(): pools2
+pools2 <- read_csv(url_csv)
+
+# Print the structure of pools1 and pools2
+str(pools1)
+# 'data.frame':	20 obs. of  4 variables:
+#  $ Name     : Factor w/ 20 levels "Acacia Ridge Leisure Centre",..: 1 2 3 4 5 6 19 7 8 9 ...
+#  $ Address  : Factor w/ 20 levels "1 Fairlead Crescent, Manly",..: 5 20 18 10 9 11 6 15 12 17 ...
+#  $ Latitude : num  -27.6 -27.6 -27.6 -27.5 -27.4 ...
+#  $ Longitude: num  153 153 153 153 153 ...
+ 
+str(pools2)
+# Classes 'tbl_df', 'tbl' and 'data.frame':	20 obs. of  4 variables:
+#  $ Name     : chr  "Acacia Ridge Leisure Centre" "Bellbowrie Pool" "Carole Park" "Centenary Pool (inner City)" ...
+#  $ Address  : chr  "1391 Beaudesert Road, Acacia Ridge" "Sugarwood Street, Bellbowrie" "Cnr Boundary Road and Waterford Road Wacol" "400 Gregory Terrace, Spring Hill" ...
+#  $ Latitude : num  -27.6 -27.6 -27.6 -27.5 -27.4 ...
+#  $ Longitude: num  153 153 153 153 153 ...
+ 
+################# Import Excel files from the web #################
+# Load the readxl and gdata package
+library(readxl)
+library(gdata)
+
+# Specification of url: url_xls
+url_xls <- "http://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/latitude.xls"
+
+# Import the .xls file with gdata: excel_gdata
+excel_gdata <- read.xls(url_xls)
+
+# You can not use read_excel() directly with a URL.
+# Complete the following instructions to work around this problem:
+# Download file behind URL, name it local_latitude.xls
+download.file(url_xls, "./local_latitude.xls")
+
+# Import the local .xls file with readxl: excel_readxl
+excel_readxl <- read_excel("local_latitude.xls")
+
+# It appears that readxl is not (yet?) able to deal with Excel files that are on the web.
+# However, a simply workaround with download.file() fixes this.
+
+#################  #################
+
+
+
+
+
+
+
+
+
+#################  #################
+
+
+
+
+
+
+
+#################  #################
