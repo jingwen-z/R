46eccc7fc91764a0a867a64d8165066354d2b3c4
jingwen <jingwen7.zheng@gmail.com>
2016-08-01 23:34:12 +0200

    Update Importing data from the web.R

--- DataCamp/Importing-Data-Into-R/Importing data from the web.R
+++ DataCamp/Importing-Data-Into-R/Importing data from the web.R
@@ -66,7 +66,129 @@ excel_readxl <- read_excel("local_latitude.xls")
 # It appears that readxl is not (yet?) able to deal with Excel files that are on the web.
 # However, a simply workaround with download.file() fixes this.
 
-#################  #################
+################# Downloading any file, secure or not #################
+# https URL to the wine RData file.
+url_rdata <- "https://s3.amazonaws.com/assets.datacamp.com/course/importing_data_into_r/wine.RData"
+
+# Download the wine file to your working directory
+download.file(url_rdata, "./wine_local.RData")
+
+# Load the wine data into your workspace using load()
+# It takes one argument, the path to the file, which is just the filename in our case.
+load("wine_local.RData")
+
+# Print out the summary of the wine data
+summary(wine)
+
+# Another way to load remote RData files is to use the url() function inside load().
+# However, this will not save the RData file to a local file.
+
+# CAN NOT directly use a URL string inside load() to load remote RData files.
+# Should use url() or download the file first using download.file().
+
+################# HTTP? httr! #################
+# Load the httr package
+library(httr)
+
+## exo 1
+
+# Get the url, save response to resp
+url <- "http://www.example.com/"
+resp <- GET(url)
+
+# Print resp
+resp
+
+# Get the raw content of resp
+# Set the `as` argument to raw
+raw_content <- content(resp, as = "raw")
+
+# Print the head of content
+head(raw_content)
+
+## exo 2
+# Get the url
+url <- "https://www.omdbapi.com/?t=Annie+Hall&y=&plot=short&r=json"
+resp <- GET(url)
+
+# Print resp
+resp
+
+# Print content of resp as text
+content(resp, as = "text")
+
+# Print content of resp
+content(resp)
+
+# `httr` converts the JSON response body automatically to an R list is very convenient.
+
+####################################### Importing data from the web: jsonlite #######################################
+
+################# From JSON to R #################
+# Load the jsonlite package
+library(jsonlite)
+
+# exo 1
+
+# Convert wine_json to a list: wine
+wine_json <- '{"name":"Chateau Migraine", "year":1997, "alcohol_pct":12.4, "color":"red", "awarded":false}'
+wine <- fromJSON(wine_json)
+
+# Import Quandl data: quandl_data
+quandl_url <- "http://www.quandl.com/api/v1/datasets/IWS/INTERNET_INDIA.json?auth_token=i83asDsiWUUyfoypkgMz"
+quandl_data <- fromJSON(quandl_url)
+
+# Print structure of wine and quandl_data
+str(wine)
+str(quandl_data)
+
+# exo 2
+
+# Experiment 1
+# Change the assignment of json1 such that the R vector after conversion contains the numbers 1 up to 6, in ascending order.
+# json1 <- '[1, 2, 4, 6]'
+json1 <- '[1, 2, 3, 4, 5, 6]'
+fromJSON(json1)
+#  [1] 1 2 3 4 5 6
+
+
+# Experiment 2
+# json2 is converted to a named list with two elements: 
+# a, containing the numbers 1, 2 and 3; b, containing the numbers 4, 5 and 6. 
+# json2 <- '{"a": [1, 2, 3]}'
+json2 <- '{"a": [1, 2, 3], "b":[4, 5, 6]}'
+fromJSON(json2)
+#  $a
+#  [1] 1 2 3
+
+#  $b
+#  [1] 4 5 6
+
+
+# Experiment 3
+# Remove characters from json3 to build a 2 by 2 matrix containing only 1, 2, 3 and 4.
+# json3 <- '[[1, 2], [3, 4], [5, 6]]'
+json3 <- '[[1, 2], [3, 4]]'
+fromJSON(json3)
+#       [,1] [,2]
+#  [1,]    1    2
+#  [2,]    3    4
+
+
+# Experiment 4
+# Add characters to json4 such that the data frame in which the json is converted contains an additional observation in the last row.
+# For this observations, a equals 5 and b equals 6. 
+# json4 <- '[{"a": 1, "b": 2}, {"a": 3, "b": 4}]'
+json4 <- '[{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}]'
+fromJSON(json4)
+#    a b
+#  1 1 2
+#  2 3 4
+#  3 5 6
+
+## Different JSON data structures will lead to different data structures in R.
+
+################# Ask OMDb #################
 
 
 
@@ -74,9 +196,7 @@ excel_readxl <- read_excel("local_latitude.xls")
 
 
 
-
-
-#################  #################
+################# From R to JSON #################
 
 
 
@@ -84,4 +204,10 @@ excel_readxl <- read_excel("local_latitude.xls")
 
 
 
-#################  #################
+################# Minify and prettify #################
+
+
+
+
+
+
