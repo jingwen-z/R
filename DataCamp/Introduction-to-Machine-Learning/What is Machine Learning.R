ddbbe9384894738c362f0055f105ed03f56ce7a6
jingwen <jingwen7.zheng@gmail.com>
2016-08-05 23:07:20 +0200

    Update What is Machine Learning.R

--- DataCamp/Introduction-to-Machine-Learning/What is Machine Learning.R
+++ DataCamp/Introduction-to-Machine-Learning/What is Machine Learning.R
@@ -94,21 +94,49 @@ plot(Petal.Length ~ Petal.Width, data = my_iris, col = kmeans_iris$cluster)
 ##################################### Supervised vs. Unsupervised #####################################
 
 ################## Getting practical with supervised learning ##################
+# Set random seed. Don't remove this line.
+set.seed(1)
 
+# Take a look at the iris dataset
+str(iris)
+summary(iris)
 
+# A decision tree model has been built for you
+tree <- rpart(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, 
+              data = iris, method = "class")
 
+# A dataframe containing unseen observations
+unseen <- data.frame(Sepal.Length = c(5.3, 7.2), 
+                     Sepal.Width = c(2.9, 3.9), 
+                     Petal.Length = c(1.7, 5.4), 
+                     Petal.Width = c(0.8, 2.3))
 
+# Predict the label of the unseen observations. Print out the result.
+predict(tree, unseen, type = "class")
 
+################## How to do unsupervised learning ##################
+# The cars data frame is pre-loaded
 
+# exo 1
+# Set random seed. Don't remove this line.
+set.seed(1)
 
+# Explore the cars dataset
+str(cars)
+summary(cars)
 
+# Group the dataset into two clusters: km_cars
+km_cars <- kmeans(cars, 2)
 
+# Print out the contents of each cluster
+km_cars$cluster
 
-##################  ##################
+# exo 2
+# Add code: color the points in the plot based on the clusters
+plot(cars, col = km_cars$cluster)
 
+# Print out the cluster centroids
+km_cars$centers
 
-
-
-
-
-
+# Replace the ___ part: add the centroids to the plot
+points(cars, pch = 22, bg = c(1, 2), cex = 2)
