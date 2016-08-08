18418015bf99ff426cf634c687784fa4acf5b4cc
jingwen <jingwen7.zheng@gmail.com>
2016-08-08 21:48:06 +0200

    final version

--- DataCamp/Introduction-to-Machine-Learning/Performance measures.R
+++ DataCamp/Introduction-to-Machine-Learning/Performance measures.R
@@ -211,41 +211,60 @@ folds <- 1 / ( (n - tr) / n )
 
 ################################# Bias and Variance #################################
 
-###############  ###############
+############### Overfitting the spam! ###############
+# The spam filter that has been 'learned' for you
+spam_classifier <- function(x){
+  prediction <- rep(NA,length(x))
+  prediction[x > 4] <- 1
+  prediction[x >= 3 & x <= 4] <- 0
+  prediction[x >= 2.2 & x < 3] <- 1
+  prediction[x >= 1.4 & x < 2.2] <- 0
+  prediction[x > 1.25 & x < 1.4] <- 1
+  prediction[x <= 1.25] <- 0
+  return(factor(prediction, levels=c("1","0")))
+}
 
+# Apply spam_classifier to emails_full: pred_full
+pred_full <- spam_classifier(emails_full$avg_capital_seq)
 
+# Build confusion matrix for emails_full: conf_full
+conf_full <- table(emails_full$spam, pred_full)
 
+# Calculate the accuracy with conf_full: acc_full
+acc_full <- sum(diag(conf_full))/sum(conf_full)
 
+# Print acc_full
+acc_full
+# 0.6561617
 
+# This hard-coded classifier gave you an accuracy of around 65% on the full dataset, 
+# which is way worse than the 100% you had on the small dataset back in chapter 1.
+# Hence, the model does not generalize at all!
 
+############### Increasing the bias ###############
+# The all-knowing classifier that has been learned for you
+# You should change the code of the classifier, simplifying it
+spam_classifier <- function(x){
+  prediction <- rep(NA,length(x))
+  prediction[x > 4] <- 1
+  prediction[x <= 4] <- 0
+  return(factor(prediction, levels=c("1","0")))
+}
 
+# conf_small and acc_small have been calculated for you
+conf_small <- table(emails_small$spam, spam_classifier(emails_small$avg_capital_seq))
+acc_small <- sum(diag(conf_small)) / sum(conf_small)
+acc_small  # 0.7692308
 
-###############  ###############
+# Apply spam_classifier to emails_full and calculate the confusion matrix: conf_full
+conf_full <- table(emails_full$spam, spam_classifier(emails_full$avg_capital_seq))
 
+# Calculate acc_full
+acc_full <- sum(diag(conf_full)) / sum(conf_full)
 
+# Print acc_full
+acc_full  # 0.7259291
 
-
-
-
-
-###############  ###############
-
-
-
-
-
-
-
-
-
-
-###############  ###############
-
-
-
-
-
-
-
-
-###############  ###############
+# Now the model no longer fits the small dataset perfectly but it fits the big dataset better.
+# I increased the bias on the model and caused it to generalize better over the complete dataset. 
+# While the first classifier overfits the data, an accuracy of 73% is far from satisfying for a spam filter.
