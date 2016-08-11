2dcdbc1a297583ca40155aa20c009f3f6815f0aa
jingwen <jingwen7.zheng@gmail.com>
2016-08-11 23:54:11 +0200

    Update Classification.R

--- DataCamp/Introduction-to-Machine-Learning/Classification.R
+++ DataCamp/Introduction-to-Machine-Learning/Classification.R
@@ -39,6 +39,81 @@ sum( diag(conf) ) / sum(conf)
 # Around 80 percent of all test instances have been classified correctly. That's not bad!
 
 ################ Pruning the tree ################
+# All packages are pre-loaded, as is the data
+
+# Calculation of a complex tree
+set.seed(1)
+tree <- rpart(Survived ~ ., train, method = "class", control = rpart.control(cp=0.00001))
+
+# Draw the complex tree
+fancyRpartPlot(tree)
+
+# Prune the tree: pruned
+pruned <- prune(tree, cp = 0.01)
+
+# Draw pruned
+fancyRpartPlot(pruned)
+
+# Another way to check if you overfit your model is 
+# by comparing the accuracy on the training set with the accuracy on the test set.
+# You'd see that the difference between those two is smaller for the simpler tree.
+
+################ Splitting criterion ################
+# All packages, emails, train, and test have been pre-loaded
+
+# Set random seed. Don't remove this line.
+set.seed(1)
+
+# Train and test tree with gini criterion
+tree_g <- rpart(spam ~ ., train, method = "class")
+pred_g <- predict(tree_g, test, type = "class")
+conf_g <- table(test$spam, pred_g)
+acc_g <- sum(diag(conf_g)) / sum(conf_g)
+
+# Change the first line of code to use information gain as splitting criterion
+tree_i <- rpart(spam ~ ., train, method = "class", parms = list(split = "information"))
+pred_i <- predict(tree_i, test, type = "class")
+conf_i <- table(test$spam, pred_i)
+acc_i <- sum(diag(conf_i)) / sum(conf_i)
+
+# Draw a fancy plot of both tree_g and tree_i
+fancyRpartPlot(tree_g)
+fancyRpartPlot(tree_i)
+
+# Print out acc_g and acc_i
+acc_g  # 0.8905797
+acc_i  # 0.8963768
+
+# using a different splitting criterion can influence the resulting model of your learning algorithm
+
+################  ################
+
+
+
+
+
+
+
+################  ################
+
+
+
+
+
+
+
+
+################  ################
+
+
+
+
+
+
+
+
+################  ################
+
 
 
 
