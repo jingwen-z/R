2973588004c449343f2bcb77724aa452d2c30a4a
jingwen <jingwen7.zheng@gmail.com>
2016-08-19 15:01:00 +0200

    k-Nearest Neighbors and Generalisation - part 2

--- DataCamp/Introduction-to-Machine-Learning/Regression.R
+++ DataCamp/Introduction-to-Machine-Learning/Regression.R
@@ -1,6 +1,6 @@
-##################################### Regression: simple and linear! #####################################
+######################## Regression: simple and linear! ########################
 
-############### Simple linear regression: your first step! ###############
+######### Simple linear regression: your first step! #########
 # The kang_nose dataset and nose_width_new are already loaded in your workspace.
 
 # Plot nose length as function of nose width.
@@ -19,9 +19,10 @@ lm_kang$coefficients
 predict(lm_kang, nose_width_new)
 #         1 
 #  703.1869 
-# With a nose width of 250mm, your prediction tells us Skippy has a nose length of around 700mm.
+# With a nose width of 250mm, your prediction tells us Skippy has a nose length of 
+# around 700mm.
 
-############### Performance measure: RMSE ###############
+######### Performance measure: RMSE ######### 
 # Build model and make plot
 lm_kang <- lm(nose_length ~ nose_width, data=kang_nose)
 plot(kang_nose, xlab = "nose width", ylab = "nose length")
@@ -38,7 +39,7 @@ rmse <- sqrt( sum(res^2) / nrow(kang_nose) )
 rmse
 # 43.26288
 
-############### Performance measures: R-squared ###############
+######### Performance measures: R-squared ######### 
 # You can compare the RMSE to the total variance of your response by calculating the R^2
 # The closer R^2 to 1, the greater the degree of linear association is between the predictor and the response variable.
 
@@ -56,7 +57,7 @@ r_sq
 summary(lm_kang)  # 0.7768914
 # An R-squared of 0.77 is pretty neat!
 
-############### Another take at regression: be critical ###############
+######### Another take at regression: be critical ######### 
 # world_bank_train and cgdp_afg is available for you to work with
 
 # Plot urb_pop as function of cgdp
@@ -76,7 +77,7 @@ summary(lm_wb)$r.squared  # 0.3822067
 # Predict the urban population of afghanistan based on cgdp_afg
 predict(lm_wb, cgdp_afg)  # 45.01204 
 
-############### Non-linear, but still linear? ###############
+######### Non-linear, but still linear? ######### 
 # world_bank_train and cgdp_afg is available for you to work with
 
 # Plot: change the formula and xlab
@@ -102,9 +103,9 @@ predict(lm_wb, cgdp_afg)  # 25.86759
 ## but leave your response variable unchanged. Overall your model still has quite a lot of unexplained variance. 
 ## If you want more precise predictions, you'll have to add other relevant variables in a multivariable linear model.
 
-##################################### Multivariable Linear Regression #####################################
+######################## Multivariable Linear Regression ######################## 
 
-############### Going all-in with predictors! ###############
+######### Going all-in with predictors! #########
 # shop_data has been loaded in your workspace
 
 # Add a plot: sales as a function of inventory. Is linearity plausible?
@@ -120,7 +121,7 @@ summary(lm_shop)
 # Multiple R-squared:  0.9932
 # Adjusted R-squared:  0.9916 
 
-############### Are all predictors relevant? ###############
+######### Are all predictors relevant? #########
 
 # exo 1
 # shop_data, shop_new and lm_shop have been loaded in your workspace
@@ -183,9 +184,9 @@ summary(lm_choco)
 
 # The predictor size is statistically insignificant, it is best to remove it.
 
-##################################### k-Nearest Neighbors and Generalization #####################################
+######################## k-Nearest Neighbors and Generalization ########################
 
-############### Does your model generalize? ###############
+######### Does your model generalize? #########
 # world_bank_train, world_bank_test and lm_wb_log are pre-loaded
 
 # Build the log-linear model
@@ -202,7 +203,8 @@ world_bank_test_input <- data.frame(cgdp = world_bank_test$cgdp)
 world_bank_test_output <- predict(lm_wb_log, world_bank_test_input)
 
 # The residuals: the difference between the ground truth and the predictions
-# Find the ground truth labels and then compare them with predictions that made with lm_wb_log.
+# Find the ground truth labels and then compare them with predictions 
+# that made with lm_wb_log.
 res_test <- world_bank_test_output - world_bank_test_truth
 
 
@@ -217,16 +219,16 @@ rmse_test / rmse_train  # 1.08308
 ## The logarithm transformation did improve your model. 
 ## It fits the data better and does a good job at generalizing!
 
-############### Your own k-NN algorithm! ###############
+######### Your own k-NN algorithm! #########
 ###
 # You don't have to change this!
 # The algorithm is already coded for you; 
 # inspect it and try to understand how it works!
 
-# x_pred: predictor values of the new observations (this will be the cgdp column of world_bank_test),
-# x: predictor values of the training set (the cgdp column of world_bank_train),
-# y: response values of the training set (the urb_pop column of world_bank_train),
-# k: the number of neighbors (this will be 30).
+# x_pred: predictor values of the new observations (the cgdp column of world_bank_test)
+# x: predictor values of the training set (the cgdp column of world_bank_train)
+# y: response values of the training set (the urb_pop column of world_bank_train)
+# k: the number of neighbors (this will be 30)
 # predict_knn: returns the predicted values for the new observations
 
 my_knn <- function(x_pred, x, y, k){
@@ -250,11 +252,11 @@ my_knn <- function(x_pred, x, y, k){
   return(predict_knn)
 }
 ###
-
 # world_bank_train and world_bank_test are pre-loaded
 
 # Apply your algorithm on the test set: test_output
-test_output <- my_knn(x_pred = world_bank_test$cgdp, x = world_bank_train$cgdp, y =world_bank_train$urb_pop, k = 30)
+test_output <- my_knn(x_pred = world_bank_test$cgdp, x = world_bank_train$cgdp, 
+                      y =world_bank_train$urb_pop, k = 30)
 
 # Have a look at the plot of the output
 plot(world_bank_train, 
@@ -262,8 +264,42 @@ plot(world_bank_train,
      ylab = "Percentage Urban Population")
 points(world_bank_test$cgdp, test_output, col = "green")
 
-############### Parametric vs non-parametric! ###############
+######### Parametric vs non-parametric! #########
+# world_bank_train and world_bank_test are pre-loaded
+# lm_wb and lm_wb_log have been trained on world_bank_train
+# The my_knn() function is available
 
+# Define ranks to order the predictor variables in the test set
+ranks <- order(world_bank_test$cgdp)
 
+# Scatter plot of test set
+plot(world_bank_test,
+     xlab = "GDP per Capita", ylab = "Percentage Urban Population")
 
+# Predict with simple linear model and add line
+test_output_lm <- predict(lm_wb, data.frame(cgdp = world_bank_test$cgdp))
+lines(world_bank_test$cgdp[ranks], test_output_lm[ranks], lwd = 2, col = "blue")
 
+# Predict with log-linear model and add line
+test_output_lm_log <- predict(lm_wb_log, data.frame(cgdp = world_bank_test$cgdp))
+lines(world_bank_test$cgdp[ranks], test_output_lm_log[ranks], lwd = 2, col = "red")
+
+# Predict with k-NN and add line
+test_output_knn <- my_knn(world_bank_test$cgdp,world_bank_train$cgdp, 
+                          world_bank_train$urb_pop, 30)
+lines(world_bank_test$cgdp[ranks], test_output_knn[ranks], lwd = 2, col = "green")
+
+## I'll calculate the RMSE of the test set for the simple linear, log-linear and k-NN regression. 
+## Have a look at the results, which regression approach performs the best?
+
+# Calculate RMSE on the test set for simple linear model
+sqrt(mean( (test_output_lm - world_bank_test$urb_pop) ^ 2))  # 17.41897
+
+# Calculate RMSE on the test set for log-linear model
+sqrt(mean( (test_output_lm_log - world_bank_test$urb_pop) ^ 2))  # 15.01911
+
+# Calculate RMSE on the test set for k-NN technique
+sqrt(mean( (test_output_knn - world_bank_test$urb_pop) ^ 2))  # 16.10026
+
+# The log-linear model seems to give the best RMSE on the test set!
+# That was all for regression. 
