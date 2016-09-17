d837ad8b3301258c2bd3d3e1cf2c34cf3092812f
jingwen <jingwen7.zheng@gmail.com>
2016-09-17 10:45:08 +0200

    All about aesthetics, part 1

--- DataCamp/Data-Visualization-with-ggplot2/Aesthetics.R
+++ DataCamp/Data-Visualization-with-ggplot2/Aesthetics.R
@@ -1,10 +1,38 @@
-#######  #######
+####### Visible Aesthetics #######
+
+### All about aesthetics ###
+
+## part 1
+# Map cyl to y
+ggplot(mtcars, aes(x = mpg, y = cyl)) + 
+geom_point()
+  
+# Map cyl to x
+ggplot(mtcars, aes(x = cyl, y = mpg)) + 
+geom_point()
+
+# Map cyl to col
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + 
+geom_point()
+
+# Change shape and size of the points in the above plot
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + 
+geom_point(shape = 1, size = 4)
+
 ###  ###
 
 
 
 
+
 ###  ###
+
+
+
+
+
+###  ###
+
 
 
 
