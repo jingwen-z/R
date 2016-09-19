49604313c0487e7fcdd1f47705a5215e381c4f14
jingwen <jingwen7.zheng@gmail.com>
2016-09-19 16:10:18 +0200

    Update and rename small-exercise/Clustering(CL).R to algorithm/Clustering(CL).R

--- algorithm/Clustering(CL).R
+++ algorithm/Clustering(CL).R
@@ -1,4 +1,4 @@
-############################## small exercise - Clustering ################################
+################ small exercise - Clustering ################
 
 reveduc <- read.table("/Users/adlz/Documents/RRRRRRR/multi data analysis/CLUSTERING (CL)/reveduc.txt", header = FALSE,
                       sep = "", row.names = 1)
@@ -6,9 +6,9 @@ colnames(reveduc) <- c("Rev","Educ")
 attach(reveduc)
 head(reveduc)
 
-###############
-### K-Means ###
-###############
+#############
+## K-Means ##
+#############
 
 rd_km <- kmeans(reveduc,3,algorithm = "MacQueen")
 # MacQueen: This algorithm works by repeatedly moving all cluster centers to the mean of their respective Voronoi sets.
@@ -26,13 +26,3 @@ points(rd_km$centers, pch = 18)
 legend(rd_km$centers[1,1], rd_km$centers[1,2], "Center_1", bty = "n", xjust = 1, yjust = 1, cex = 0.8)
 legend(rd_km$centers[2,1], rd_km$centers[2,2], "Center_2", bty = "n", xjust = 0, yjust = 0.5, cex = 0.8)
 legend(rd_km$centers[3,1], rd_km$centers[3,2], "Center_3", bty = "n", xjust = 0, yjust = 0.5, cex = 0.8)
-
-
-
-
-
-
-
-
-
-
