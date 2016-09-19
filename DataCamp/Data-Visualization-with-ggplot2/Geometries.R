21a09cc1d42119a4ae47e44828291a7d9e17906c
jingwen <jingwen7.zheng@gmail.com>
2016-09-19 21:48:32 +0200

    Update Geometries.R

--- DataCamp/Data-Visualization-with-ggplot2/Geometries.R
+++ DataCamp/Data-Visualization-with-ggplot2/Geometries.R
@@ -71,6 +71,119 @@ geom_histogram(aes(y = ..density..), binwidth = 1, fill = myBlue)
 # fill: place the bars on top of each other, but this time use proportions. 
 # dodge: place the bars next to each other. Counts are used.
 
+# Draw a bar plot of cyl, filled according to am
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+geom_bar()
+
+# Change the position argument to stack
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+geom_bar(position = "stack")
+
+# Change the position argument to fill
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+geom_bar(position = "fill")
+
+# Change the position argument to dodge
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+geom_bar(position = "dodge")
+
+### Overlapping bar plots ###
+# Draw a bar plot of cyl, filled according to am
+ggplot(mtcars, aes(x = cyl, fill = am)) + 
+geom_bar()
+
+# Change the position argument to "dodge"
+ggplot(mtcars, aes(x = cyl, fill = am)) + 
+geom_bar(position = "dodge")
+
+# Define posn_d with position_dodge()
+posn_d <- position_dodge(width = 0.2)
+
+# Change the position argument to posn_d
+ggplot(mtcars, aes(x = cyl, fill = am)) + 
+geom_bar(position = posn_d)
+
+# Use posn_d as position and adjust alpha to 0.6
+ggplot(mtcars, aes(x = cyl, fill = am)) + 
+geom_bar(position = posn_d, alpha = 0.6)
+
+### Overlapping histograms (1) ###
+# A basic histogram, add coloring defined by cyl 
+ggplot(mtcars, aes(mpg, fill = cyl)) +
+  geom_histogram(binwidth = 1)
+
+# Change position to identity 
+ggplot(mtcars, aes(mpg, fill = cyl)) +
+  geom_histogram(binwidth = 1, position = "identity")
+
+# Change geom to freqpoly (position is identity by default) 
+ggplot(mtcars, aes(mpg, col = cyl)) +
+  geom_histogram(binwidth = 1, position = "identity") +
+  geom_freqpoly(binwidth = 1)
+
+### Bar plots with color ramp ###
+
+## part 1
+# Example of how to use a brewed color palette
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Set1")
+
+# Use str() on Vocab to check out the structure
+str(Vocab)
+
+# Plot education on x and vocabulary on fill
+# Use the default brewed color palette
+ggplot(Vocab, aes(x = education, fill = vocabulary)) +
+geom_bar(position = "fill") +
+scale_fill_brewer()
+
+## part 2
+# Final plot of last exercise
+ggplot(Vocab, aes(x = education, fill = vocabulary)) +
+  geom_bar(position = "fill") +
+  scale_fill_brewer()
+  
+# Definition of a set of blue colors
+blues <- brewer.pal(9, "Blues")
+
+# Make a color range using colorRampPalette() and the set of blues
+blue_range <- colorRampPalette(blues)
+
+# Use blue_range to adjust the color of the bars, use scale_fill_manual()
+ggplot(Vocab, aes(x = education, fill = vocabulary)) +
+geom_bar(position = "fill") +
+scale_fill_manual(values = blue_range(11))
+
+### Overlapping histograms (2) ###
+# Basic histogram plot command
+ggplot(mtcars, aes(mpg)) + 
+  geom_histogram(binwidth = 1)
+
+# Expand the histogram to fill using am
+ggplot(mtcars, aes(mpg, fill = am)) + 
+  geom_histogram(binwidth = 1)
+
+# Change the position argument to "dodge"
+ggplot(mtcars, aes(mpg, fill = am)) + 
+  geom_histogram(binwidth = 1, position = "dodge")
+
+# Change the position argument to "fill"
+ggplot(mtcars, aes(mpg, fill = am)) + 
+  geom_histogram(binwidth = 1, position = "fill")
+
+# Change the position argument to "identity" and set alpha to 0.4
+ggplot(mtcars, aes(mpg, fill = am)) + 
+  geom_histogram(binwidth = 1, position = "identity", alpha = 0.4)
+
+# Change fill to cyl
+ggplot(mtcars, aes(mpg, fill = cyl)) + 
+  geom_histogram(binwidth = 1, position = "identity", alpha = 0.4)
+
+###  ###
+
+
+
 
 
 
@@ -78,8 +191,6 @@ geom_histogram(aes(y = ..density..), binwidth = 1, fill = myBlue)
 
 
 
-
-###  ###
 
 
 
