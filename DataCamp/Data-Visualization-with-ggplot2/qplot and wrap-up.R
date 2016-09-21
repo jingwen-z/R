e7afbaf154ed346db1a3eb585c1d8da74876e2fd
jingwen <jingwen7.zheng@gmail.com>
2016-09-21 23:31:13 +0200

    Using qplot

--- DataCamp/Data-Visualization-with-ggplot2/qplot and wrap-up.R
+++ DataCamp/Data-Visualization-with-ggplot2/qplot and wrap-up.R
@@ -1,1 +1,77 @@
+####### qplot #######
 
+### Using qplot ###
+# The old way (shown)
+plot(mpg ~ wt, data = mtcars)
+
+# Using ggplot:
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+geom_point()
+
+# Using qplot:
+qplot(wt, mpg, data = mtcars)
+
+### Using aesthetics ###
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+###  ###
