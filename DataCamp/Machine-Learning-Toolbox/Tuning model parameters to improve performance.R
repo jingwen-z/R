7528eb12f4a16e99829d1bfd1b2573a3ee065d67
jingwen <jingwen7.zheng@gmail.com>
2016-09-25 11:24:32 +0200

    Random forests and wine

---
+++ DataCamp/Machine-Learning-Toolbox/Tuning model parameters to improve performance.R
@@ -0,0 +1,94 @@
+####### Random forests and wine #######
+
+### Fit a random forest ###
+# Fit random forest: model
+model <- train(
+  quality ~ .,
+  tuneLength = 1,
+  data = wine, method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, verboseIter = TRUE)
+)
+
+# Print model to console
+model
+
+# Random Forest 
+#
+# 100 samples
+#  12 predictor
+#
+# No pre-processing
+# Resampling: Cross-Validated (5 fold) 
+# Summary of sample sizes: 80, 80, 80, 80, 80 
+# Resampling results:
+#
+#   RMSE       Rsquared 
+#   0.6619301  0.2878064
+#
+# Tuning parameter 'mtry' was held constant at a value of 3
+
+####### Explore a wider model space #######
+
+###  ###
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+#######  #######
+
+###  ###
+
+
+
+
+
+###  ###
+
+
+###  ###
+
+
+
+
+
+#######  #######
+
+###  ###
+
+
+
+
+
+###  ###
+
+
+
+
+
+
+#######  #######
+
+###  ###
+
+
+
+
+
+###  ###
+
+
+
+
+
+###  ###
+
+
+
