a2c8adae78c2cd8dd2060791a26f046d086877ba
jingwen <jingwen7.zheng@gmail.com>
2016-09-25 09:52:10 +0200

    AUC - Customizing trainControl

--- DataCamp/Machine-Learning-Toolbox/Classification models: fitting them and evaluating their performance.R
+++ DataCamp/Machine-Learning-Toolbox/Classification models: fitting them and evaluating their performance.R
@@ -106,12 +106,16 @@ colAUC(p, test[["Class"]], plotROC = TRUE)
 ####### Area under the curve #######
 
 ### Customizing trainControl ###
+# Create trainControl object: myControl
+myControl <- trainControl(
+  method = "cv",
+  number = 10,
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, # IMPORTANT!
+  verboseIter = TRUE
+)
 
-
-
-
-
-###  ###
+### Using custom trainControl ###
 
 
 
