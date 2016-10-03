a7d7038951b8af2fd7e261f9561f2a1b57379402
jingwen <jingwen7.zheng@gmail.com>
2016-10-03 23:55:14 +0200

    Create Selecting models: a case study in churn prediction.R

---
+++ DataCamp/Machine-Learning-Toolbox/Selecting models: a case study in churn prediction.R
@@ -0,0 +1,63 @@
+####### Reusing a trainControl #######
+
+### Make custom train/test indices ###
+# Create custom indices: myFolds
+myFolds <- createFolds(churn_y, k = 5)
+
+# Create reusable trainControl object: myControl
+myControl <- trainControl(
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, # IMPORTANT!
+  verboseIter = TRUE,
+  savePredictions = TRUE,
+  index = myFolds
+)
+
+####### Reintroduce glmnet #######
+
+### Fit the baseline model ###
+# Fit glmnet model: model_glmnet
+model_glmnet <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "glmnet",
+  trControl = myControl
+)
+
+####### Reintroduce random forest #######
+
+### Random forest with custom trainControl ###
+# Fit random forest: model_rf
+model_rf <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "ranger",
+  trControl = myControl
+)
+
+####### Comparing models #######
+
+### Create a resamples object ###
+# Create model_list
+model_list <- list(glmnet = model_glmnet, ranger = model_rf)
+
+# Pass model_list to resamples(): resamples
+resamples <- resamples(model_list)
+
+# Summarize the results
+summary(resamples)
+
+#######  #######
+
+###  ###
+
+
+
+###  ###
+
+
+
+
+###  ###
+
+
