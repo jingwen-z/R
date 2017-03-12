95de8fb16ccf9e7edd757ea3124a8a60183923c4
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-03-12 22:47:57 +0100

    MovieLens Project Part 3

--- algorithm/MovieLens.R
+++ algorithm/MovieLens.R
@@ -1,7 +1,8 @@
 # Installation
 #
-# Uncomment the following lines to install the required packages:
-# install.packages("class") install.packages("sampling")
+if (!require("pacman"))
+  install.packages("pacman")
+pacman::p_load("class", "sampling")
 
 library(class)
 library(sampling)
@@ -10,48 +11,49 @@ library(sampling)
 #
 # Datasets are acquired from website https://grouplens.org/datasets/movielens
 setwd("Documents/R/datasets/ml-100k")
-data <- read.table("u.data")[ , -4]
-names(data) <- c("UserID", "MovieID", "Rating")
+rating <- read.table("u.data")[ , -4]
+names(rating) <- c("UserID", "MovieID", "Rating")
 
 # Compile the function "MovieLensKNN"
-MovieLensKNN <- function(userID, movieID, n, K) {
-  targetUsers <- which(data$UserID == userID)
-  relativeMovies <- sample(targetUsers, min(length(targetUsers), n))
+MovieLensKNN <- function(userID, movieID, n, k) {
+  targetUserIndexes <- which(rating$UserID == userID)
+  sampleIndexes <- sample(targetUserIndexes, min(length(targetUserIndexes), n))
 
-  relativeMoviesID <- data$MovieID[relativeMovies]
-  targetMoviesID <- movieID
+  sampleMovieIDs <- rating$MovieID[sampleIndexes]
+  targetMovieID <- movieID
 
-  trainingMovies <- which(data$MovieID == targetMoviesID)
-  trainingUsers <- data$UserID[trainingMovies[-1]]
+  targetMovieIndexes <- which(rating$MovieID == targetMovieID)
+  targetUser <- which(rating$UserID[targetMovieIndexes] == userID)
+  trainingUsers <- rating$UserID[targetMovieIndexes][-targetUser]
 
-  dataAll <- matrix(0, 1+length(trainingUsers), 2+length(relativeMoviesID))
-  dataAll <- data.frame(dataAll)
-  names(dataAll) <- c("userID",
+  dataMatrix <- matrix(0, 1+length(trainingUsers), 2+length(sampleMovieIDs))
+  dataDF <- data.frame(dataMatrix)
+  names(dataDF) <- c("userID",
                       paste("targetMovieID-", movieID),
-                      paste("tuNotedMovieID-", relativeMoviesID, sep = ""))
-  movies <- c(targetMoviesID, relativeMoviesID)
-  dataAll$userID <- c(userID, trainingUsers)
+                      paste("sampleMovieID-", sampleMovieIDs, sep = ""))
+  movies <- c(targetMovieID, sampleMovieIDs)
+  dataDF$userID <- c(userID, trainingUsers)
 
-  for (i in 1:nrow(dataAll)) {
-    dataUsers <- data[which(data$UserID == dataAll$userID[i]), ]
+  for (i in 1:nrow(dataDF)) {
+    dataUsers <- rating[which(rating$UserID == dataDF$userID[i]), ]
 
     for (j in 1:length(movies)) {
       note <- dataUsers$MovieID == movies[j]
 
       if (sum(as.numeric(note)) != 0) {
-        dataAll[i, j+1] = dataUsers$Rating[which(note)]
+        dataDF[i, j+1] = dataUsers$Rating[which(note)]
       }
     }
   }
 
-  testDataX <- dataAll[1, c(-1, -2)]
-  testDataY <- dataAll[1, 2]
-  trainingDataX <- dataAll[-1, c(-1, -2)]
-  trainingDataY <- dataAll[-1, 2]
+  testDataX <- dataDF[1, c(-1, -2)]
+  testDataY <- dataDF[1, 2]
+  trainingDataX <- dataDF[-1, c(-1, -2)]
+  trainingDataY <- dataDF[-1, 2]
 
-  fit <- knn(trainingDataX, testDataX, cl = trainingDataY, k = K)
+  fit <- knn(trainingDataX, testDataX, cl = trainingDataY, k = k)
 
-  list("All Data:" = dataAll,
+  list("Data Framework:" = dataDF,
        "True Rating:" = testDataY,
        "Predict Rating" = fit,
        "UserID:" = userID,
