5e53fbdd33f5e242bf032b37c4e83ccd858ef627
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-06-14 16:16:16 +0200

    reformat script

--- TSE/internship/project_AFD.R
+++ TSE/internship/project_AFD.R
@@ -15,12 +15,12 @@ library(sampling)
 
 # import dataset
 datasetRaw <- read.delim("Documents/R/afd/guarantee_activation.txt",
-                                    header = TRUE,
-                                    sep    = ";",
-                                    dec    = ",")
+                         header = TRUE,
+                         sep    = ";",
+                         dec    = ",")
 
 # extraction data
-dataset <- datasetRaw[ , c(73, 7, 5, 8, 11, 10, 13, 12, 17, 19, 21)]
+dataset <- datasetRaw[, c(73, 7, 5, 8, 11, 10, 13, 12, 17, 19, 21)]
 View(dataset)
 
 colnames(dataset)<- c("compensation",
@@ -35,21 +35,18 @@ colnames(dataset)<- c("compensation",
                       "guaranteeAmount",
                       "exchangeRate")
 
-str(dataset)
-summary(dataset)
-
 # convert data.frame columns from factors to characters
 factorCols <- sapply(dataset, is.factor)
 dataset[factorCols] <- lapply(dataset[factorCols], as.character)
 
 # check missing values
-any(is.na(dataset$compensation))  # TRUE
+any(is.na(dataset$compensation))
 
 # count number of NAs
-sum(is.na(dataset$compensation))  # 36
+sum(is.na(dataset$compensation))
 
 # subset data, keeping only complete cases
-dataset <- dataset[complete.cases(dataset$compensation), ]  # 260 obs.
+dataset <- dataset[complete.cases(dataset$compensation), ]
 
 # supplement missing values
 dataset$primaryBeneficiaryID[dataset$primaryBeneficiary == "BOA Madagascar"] <- "500750"
@@ -69,7 +66,7 @@ dataset$loanAmount <- dataset$loanAmount / 1000
 dataset$guaranteeAmount <- dataset$guaranteeAmount / 1000
 
 # remove outlier 0 in "compensation"
-dataset <- dataset[!(dataset$compensation == 0), ]  # 166 obs.
+dataset <- dataset[!(dataset$compensation == 0), ]
 
 #--------------------------------------
 # data visualisation
@@ -102,36 +99,24 @@ ggplot(dataset, aes(x = loanAmount, y = compensation)) +
 
 # The independent variables are guaranteeType, country, primaryBeneficiary, 
 # loanAmount, guaranteeAmount, pourcentage_garantie, exchangeRate.
-linearModel <- lm(compensation ~ factor(guaranteeType) 
-                 + factor(country)
-                 + factor(primaryBeneficiary)
-                 + loanAmount
-                 + guaranteeAmount
-                 + exchangeRate,
-               data = dataset)
+linearModel <- lm(compensation ~ factor(guaranteeType)
+                  + factor(country)
+                  + factor(primaryBeneficiary)
+                  + loanAmount
+                  + guaranteeAmount
+                  + exchangeRate,
+                  data = dataset)
 summary(linearModel)
-
-# According to the summary, R-squared is 0.8982.
-# The following values are statictically significant at different levels:
-#   MAURICE: At 1%.
-#   BOA Bénin: At 1%.
-#   Banque des MASCAREIGNES: At 1%.
-#   MCB Ltd: At 1%.
-#   ALIOS: At 5%.
-#   CREDIT DU SENEGAL: At 10%.
-#   loanAmount: At 10%.
 
 # check if the observations are independent / have no pattern
 plot(linearModel$fitted.values, linearModel$residuals, col = 4)
+
 # check if all errors are normally distributed
 qqnorm(linearModel$residuals, col = 4)
 
 #--------------------------------------
 # Decision trees (CART)
 #--------------------------------------
-
-str(dataset)
-summary(dataset)
 
 compensationGroup <- matrix(0, 166, 1)
 compensationGroup[which(dataset$compensation < 53)] <- "A"
@@ -143,7 +128,6 @@ dataset$compensationGroup <- compensationGroup
 dataset[1:10, c(1, 12)]
 
 # for each group, calculate number of observations in test set
-
 a <- round(1 / 4 * sum(dataset$compensationGroup == "A"))
 b <- round(1 / 4 * sum(dataset$compensationGroup == "B"))
 c <- round(1 / 4 * sum(dataset$compensationGroup == "C"))
@@ -151,30 +135,30 @@ c <- round(1 / 4 * sum(dataset$compensationGroup == "C"))
 # stratified sampling
 sub <- strata(dataset,
               stratanames = "compensationGroup",
-              size = c(a,b,c),
+              size = c(a, b, c),
               method = "srswor")
 sub[1:10, ]
 
 trainDataset <- dataset[-sub$ID_unit, ]
 testDataset <- dataset[sub$ID_unit, ]
 
-nrow(trainDataset)  # 125
-nrow(testDataset)  # 41
+nrow(trainDataset)
+nrow(testDataset)
 
 # Regression Tree
 regrTree <- rpart(compensation ~ guaranteeType
-                    + country
-                    + primaryBeneficiary
-                    + loanAmount,
+                  + country
+                  + primaryBeneficiary
+                  + loanAmount,
                   trainDataset,
                   method = "anova")
 rpart.plot(regrTree, type = 4)
 
 # Classification Tree
 classTree <- rpart(compensationGroup ~ guaranteeType
-                    + country
-                    + primaryBeneficiary
-                    + loanAmount, 
+                   + country
+                   + primaryBeneficiary
+                   + loanAmount, 
                    trainDataset,
                    method = "class")
 rpart.plot(classTree, type = 4)
