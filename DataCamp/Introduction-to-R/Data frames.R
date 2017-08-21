42b0c6a41863d7ddf54ed74c0b6c4990082898b7
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-08-21 19:19:19 +0200

    remove useless comment

--- DataCamp/Introduction-to-R/Data frames.R
+++ DataCamp/Introduction-to-R/Data frames.R
@@ -20,22 +20,20 @@ rotation <- c(58.64, -243.02, 1, 1.03, 0.41, 0.43, -0.72, 0.67)
 rings <- c(FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE)
 
 # Create a data frame from the vectors
-planets_df <- data.frame(name,type,diameter,rotation,rings)
+planets_df <- data.frame(name, type, diameter, rotation, rings)
 
 # Check the structure of planets_df
 str(planets_df)
 
 ############# Selection of data frame elements #############
-# The planets_df data frame from the previous exercise is pre-loaded
-planets_df
 # Print out diameter of of Mercury (row 1, column 3)
-planets_df[1,3]
+planets_df[1, 3]
 
 # Print out data for Mars (entire fourth row)
-planets_df[4,]
+planets_df[4, ]
 
 # Select first 5 values of diameter column
-planets_df[1:5,"diameter"]
+planets_df[1:5, "diameter"]
 
 ############# Only planets with rings #############
 # Select the rings variable from planets_df
@@ -45,7 +43,7 @@ rings_vector <- planets_df$rings
 rings_vector
 
 # Adapt the code to select all columns for planets with rings
-planets_df[rings_vector,]
+planets_df[rings_vector, ]
 
 ############# Only planets with rings but shorter #############
 # Select planets with diameter < 1
@@ -56,4 +54,4 @@ subset(planets_df, subset = diameter < 1)
 positions <- order(planets_df$diameter)
 
 # Use positions to sort planets_df
-planets_df[positions,]
+planets_df[positions, ]
