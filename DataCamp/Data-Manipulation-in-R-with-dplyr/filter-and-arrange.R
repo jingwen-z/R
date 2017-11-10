083d568bc0cd0a7b09ebb6a3c1fb2984224c4174
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-10 12:43:31 +0100

    filter()

---
+++ DataCamp/Data-Manipulation-in-R-with-dplyr/filter-and-arrange.R
@@ -0,0 +1,34 @@
+## filter
+# Logical operators
+# All flights that traveled 3000 miles or more
+filter(hflights, Distance > 3000)
+
+# All flights flown by one of JetBlue, Southwest, or Delta
+filter(hflights, UniqueCarrier %in% c("JetBlue", "Southwest", "Delta"))
+
+# All flights where taxiing took longer than flying
+filter(hflights, TaxiIn + TaxiOut > AirTime)
+
+# Combining tests using boolean operators
+# All flights that departed before 5am or arrived after 10pm
+filter(hflights, DepTime < 500 | ArrTime > 2200)
+
+# All flights that departed late but arrived ahead of schedule
+filter(hflights, DepDelay > 0 & ArrDelay < 0)
+
+# All flights that were cancelled after being delayed
+filter(hflights, Cancelled == 1, DepDelay > 0)
+
+# Blend together what you've learned!
+# Select the flights that had JFK as their destination: c1
+c1 <- filter(hflights, Dest == "JFK")
+
+# Combine the Year, Month and DayofMonth variables to create a Date column: c2
+c2 <- mutate(c1, Date = paste(Year, Month, DayofMonth, sep = "-"))
+
+# Print out a selection of columns of c2
+select(c2, Date, DepTime, ArrTime, TailNum)
+
+# How many weekend flights flew a distance of more than 1000 miles
+# but had a total taxiing time below 15 minutes?
+filter(hflights, DayOfWeek %in% c(6,7), Distance > 1000, TaxiIn + TaxiOut < 15)
