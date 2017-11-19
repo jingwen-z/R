0f1ead08b2e07b0bee861c7b8771478cf09c3d93
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-19 12:34:42 +0100

    reformat codes

--- DataCamp/Data-Visualization-with-ggplot2/Data.R
+++ DataCamp/Data-Visualization-with-ggplot2/Data.R
@@ -2,7 +2,7 @@ library(tidyr)
 
 ########## Objects and Layers ##########
 
-##### base package and ggplot2, part 1 - plot #####
+##### base package and ggplot2, part 1 - plot
 # Plot the correct variables of mtcars
 plot(mtcars$wt, mtcars$mpg, col = mtcars$cyl)
 
@@ -12,7 +12,7 @@ mtcars$cyl <- as.factor(mtcars$cyl)
 # Make the same plot as in the first instruction
 plot(mtcars$wt, mtcars$mpg, col = mtcars$cyl)
 
-##### base package and ggplot2, part 2 - lm #####
+##### base package and ggplot2, part 2 - lm
 # Basic plot
 mtcars$cyl <- as.factor(mtcars$cyl)
 plot(mtcars$wt, mtcars$mpg, col = mtcars$cyl)
@@ -24,21 +24,18 @@ carModel <- lm(mpg ~ wt, data = mtcars)
 abline(reg = carModel, lty = 2)
 
 # Plot each subset efficiently with lapply
-# You don't have to edit this code
 lapply(mtcars$cyl, function(x) {
   abline(carModel, col = x)
   })
 
 # This code will draw the legend of the plot
-# You don't have to edit this code
 legend(x = 5, y = 33, legend = levels(mtcars$cyl),
        col = 1:3, pch = 1, bty = "n")
 
-##### base package and ggplot2, part 3 #####
-# Convert cyl to factor (don't need to change)
+##### base package and ggplot2, part 3
 mtcars$cyl <- as.factor(mtcars$cyl)
 
-# Example from base R (don't need to change)
+# Example from base R
 plot(mtcars$wt, mtcars$mpg, col = mtcars$cyl)
 abline(lm(mpg ~ wt, data = mtcars), lty = 2)
 lapply(mtcars$cyl, function(x) {
@@ -53,19 +50,19 @@ ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
 
 # Plot 2: include the lines of the linear models, per cyl
 ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
-  geom_point() + # Copy from Plot 1
+  geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
 # Plot 3: include a lm for the entire dataset in its whole
 ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
-  geom_point() + # Copy from Plot 2
+  geom_point() +
   geom_smooth(method = "lm", se = FALSE, linetype = 2) +
   geom_smooth(aes(group = 1))
 
 ########## Tidy Data ##########
 
-##### Variables to visuals, part 1 #####
-# Consider the structure of iris, iris.wide and iris.tidy (in that order)
+##### Variables to visuals, part 1
+# Consider the structure of iris, iris.wide and iris.tidy
 str(iris)
 str(iris.wide)
 str(iris.tidy)
@@ -76,29 +73,26 @@ ggplot(iris.tidy, aes(x = Species, y = Value, col = Part)) +
   geom_jitter() +
   facet_grid(. ~ Measure)
 
-##### Variables to visuals, part 1b #####
-# Fill in the ___ to produce to the correct iris.tidy dataset
+##### Variables to visuals, part 1b
 iris.tidy <- iris %>%
   gather(iris, value = measurement, -Species) %>%
   separate(iris, c("Part", "Measure"), "\\.")
 
-##### Variables to visuals, part 2 #####
-# Consider the head of iris, iris.wide and iris.tidy (in that order)
+##### Variables to visuals, part 2
+# Consider the head of iris, iris.wide and iris.tidy
 head(iris)
 head(iris.wide)
 head(iris.tidy)
 
 # Think about which dataset you would use to get the plot shown right
-# Fill in the ___ to produce the plot given to the right
 ggplot(iris.wide, aes(x = Length, y = Width, col = Part)) +
   geom_jitter() +
   facet_grid(. ~ Species)
 
-##### Variables to visuals, part 2b #####
-# Add column with unique ids (don't need to change)
+##### Variables to visuals, part 2b
+# Add column with unique ids
 iris$Flower <- 1:nrow(iris)
 
-# Fill in the ___ to produce to the correct iris.wide dataset
 iris.wide <- iris %>%
   gather(iris, value = value, -Species, -Flower) %>%
   separate(iris,c("Part", "Measure"), "\\.") %>%
