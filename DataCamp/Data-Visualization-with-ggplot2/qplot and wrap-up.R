3311f2a2f651292678a2d459c34e3ec544147a29
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-23 12:34:42 +0100

    reformat codes

--- DataCamp/Data-Visualization-with-ggplot2/qplot and wrap-up.R
+++ DataCamp/Data-Visualization-with-ggplot2/qplot and wrap-up.R
@@ -1,7 +1,6 @@
 ####### qplot #######
 
-### Using qplot ###
-# The old way (shown)
+### Using qplot
 plot(mpg ~ wt, data = mtcars)
 
 # Using ggplot:
@@ -11,11 +10,11 @@ geom_point()
 # Using qplot:
 qplot(wt, mpg, data = mtcars)
 
-### Using aesthetics ###
-# basic scatter plot:
+### Using aesthetics
+# basic scatter plot
 qplot(x = wt, y = mpg, data = mtcars)
 
-# Categorical:
+# Categorical
 # cyl
 qplot(x = wt, y = mpg, data = mtcars, size = cyl)
 
@@ -29,7 +28,7 @@ qplot(x = wt, y = mpg, data = mtcars, col = hp)
 # qsec
 qplot(x = wt, y = mpg, data = mtcars, col = qsec)
 
-### Choosing geoms ###
+### Choosing geoms
 ## part 1
 # qplot() with x only
 qplot(x = factor(cyl), data = mtcars)
@@ -46,41 +45,26 @@ ggplot(mtcars, aes(cyl, wt, fill = factor(am))) +
 geom_dotplot(stackdir = "center", binaxis = "y")
 
 # qplot with geom "dotplot", binaxis = "y" and stackdir = "center"
-qplot(x = cyl, y = wt, data = mtcars, fill = factor(am), 
-      geom = "dotplot", stackdir = "center", binaxis = "y") 
+qplot(x = cyl, y = wt, data = mtcars, fill = factor(am),
+      geom = "dotplot", stackdir = "center", binaxis = "y")
 
 ####### Wrap-up #######
 
-### Chicken weight ###
-# ChickWeight is available in your workspace
-
-# Check out the head of ChickWeight
-head(ChickWeight)
-
-# Use ggplot() for the second instruction
+### Chicken weight
 ggplot(ChickWeight, aes(x = Time, y = weight)) +
 geom_line(aes(group = Chick))
 
-# Use ggplot() for the third instruction
 ggplot(ChickWeight, aes(x = Time, y = weight, col = Diet)) +
 geom_line(aes(group = Chick))
 
-# Use ggplot() for the last instruction
 ggplot(ChickWeight, aes(x = Time, y = weight, col = Diet)) +
 geom_line(aes(group = Chick), alpha = 0.3) +
 geom_smooth(lwd = 2, se = FALSE)
 
-### Titanic ###
-# titanic is avaliable in your workspace
-
-# Check out the structure of titanic
-str(titanic)
-
-# Use ggplot() for the first instruction
+### Titanic
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
 geom_bar(position = "dodge")
 
-# Use ggplot() for the second instruction
 ggplot(titanic, aes(x = factor(Pclass), fill = factor(Sex))) +
 geom_bar(position = "dodge") +
 facet_grid(. ~ Survived)
@@ -88,7 +72,6 @@ facet_grid(. ~ Survived)
 # Position jitter (use below)
 posn.j <- position_jitter(0.5, 0)
 
-# Use ggplot() for the last instruction
 ggplot(titanic, aes(x = factor(Pclass), y = Age, col = factor(Sex))) +
 geom_jitter(size = 3, alpha = 0.5, position = posn.j) +
 facet_grid(. ~ Survived)
