ef540f52b57947d6d282348130d621fbcb04acfd
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-24 12:34:42 +0100

    reformat codes

--- DataCamp/Data-Visualization-with-ggplot2/Introduction.R
+++ DataCamp/Data-Visualization-with-ggplot2/Introduction.R
@@ -1,9 +1,9 @@
 # Load the ggplot2 package
 library(ggplot2)
 
-########## Introduction ##########
+###### Introduction ######
 
-##### Exploring ggplot2, part 1 #####
+##### Exploring ggplot2, part 1
 # Explore the mtcars data frame with str()
 str(mtcars)
 
@@ -11,50 +11,39 @@ str(mtcars)
 ggplot(mtcars, aes(x = cyl, y = mpg)) +
   geom_point()
 
-# The point geom is used to create scatterplots.
-
-# The plot isn't really satisfying. 
-# Although cyl (the number of cylinders) is categorical, it is classified as numeric in mtcars. 
-# We have to explicitly tell ggplot2 that cyl is a categorical variable.
-
-##### Exploring ggplot2, part 2 #####
+##### Exploring ggplot2, part 2
 # Change the command below so that cyl is treated as factor
 ggplot(mtcars, aes(x = factor(cyl), y = mpg)) +
   geom_point()
 
-########## Grammar of Graphics ##########
+###### Grammar of Graphics ######
 
-##### Exploring ggplot2, part 3 #####
-# A scatter plot has been made for you
+##### Exploring ggplot2, part 3
 ggplot(mtcars, aes(x = wt, y = mpg)) +
   geom_point()
 
-# Replace ___ with the correct vector
-# The color is dependent on the displacement of the car engine, found in disp.
 ggplot(mtcars, aes(x = wt, y = mpg, col = disp)) +
   geom_point()
 
-# Replace ___ with the correct vector
-# The size is dependent on the displacement of the car engine, found in disp.
 ggplot(mtcars, aes(x = wt, y = mpg, size = disp)) +
   geom_point()
 
-########## ggplot2 ##########
+###### ggplot2 ######
 
-##### Exploring ggplot2, part 4 #####
+##### Exploring ggplot2, part 4
 # Explore the diamonds data frame with str()
 str(diamonds)
 
 # Add geom_point() with +
-ggplot(diamonds, aes(x = carat, y = price)) + 
+ggplot(diamonds, aes(x = carat, y = price)) +
     geom_point()
 
 # Add geom_point() and geom_smooth() with +
-ggplot(diamonds, aes(x = carat, y = price)) + 
-    geom_point() + 
+ggplot(diamonds, aes(x = carat, y = price)) +
+    geom_point() +
     geom_smooth()
 
-##### Exploring ggplot2, part 5 #####
+##### Exploring ggplot2, part 5
 # The plot you created in the previous exercise
 ggplot(diamonds, aes(x = carat, y = price)) +
   geom_point() +
@@ -68,12 +57,11 @@ ggplot(diamonds, aes(x = carat, y = price)) +
 ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
   geom_smooth()
 
-# Keep the color settings from previous command. Plot only the points with argument alpha.
 # alpha = 0.4 makes the points 40% transparent
 ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
   geom_point(alpha = 0.4)
 
-##### Understanding the grammar, part 1 #####
+##### Understanding the grammar, part 1
 # Create the object containing the data and aes layers: dia_plot
 dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
 
@@ -83,7 +71,7 @@ dia_plot + geom_point()
 # Add the same geom layer, but with aes() inside
 dia_plot + geom_point(aes(col = clarity))
 
-##### Understanding the grammar, part 2 #####
+##### Understanding the grammar, part 2
 set.seed(1)
 
 # The dia_plot object has been created for you
@@ -94,6 +82,4 @@ dia_plot <- dia_plot + geom_point(alpha = 0.2)
 
 # Plot dia_plot with additional geom_smooth() with se set to FALSE
 dia_plot + geom_smooth(se = FALSE)
-
-# Copy the command from above and add aes() with the correct mapping to geom_smooth()
 dia_plot + geom_smooth(aes(col = clarity), se = FALSE)
