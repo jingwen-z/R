ca44f49f58ca9c4d4582e4f03b2dd853c39c0160
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-27 12:34:42 +0100

    reformat codes

--- DataCamp/Importing-Data-Into-R/Importing data from flat files.R
+++ DataCamp/Importing-Data-Into-R/Importing data from flat files.R
@@ -1,41 +1,41 @@
-######################################## Introduction & Flat Files ########################################
+library(data.table)
+library(readr)
 
-############# read.csv #############
-# Import swimming_pools.csv: pools
-pools <- read.csv("swimming_pools.csv", header = TRUE, sep = ",", dec = ".", stringsAsFactors = TRUE)
+##### Introduction & Flat Files #####
 
-# Print the structure of pools
+### read.csv ###
+pools <- read.csv("swimming_pools.csv", header = TRUE,
+                  sep = ",", dec = ".",
+                  stringsAsFactors = TRUE)
 str(pools)
 
-############# read.delim #############
+### read.delim ###
 # Import hotdogs.txt: hotdogs
-hotdogs <- read.delim("hotdogs.txt", header = FALSE, sep = "\t", stringsAsFactors = TRUE)
-
-# Summarize hotdogs
+hotdogs <- read.delim("hotdogs.txt", header = FALSE,
+                      sep = "\t", stringsAsFactors = TRUE)
 summary(hotdogs)
 
-############# read.table #############
+### read.table ###
 # Path to the hotdogs.txt file: path
 path <- file.path("data", "hotdogs.txt")
 
 # Import the hotdogs.txt file: hotdogs
-hotdogs <- read.table(path, 
-                      sep = "\t", 
+hotdogs <- read.table(path,
+                      sep = "\t",
                       col.names = c("type", "calories", "sodium"))
-
-# Call head() on hotdogs
 head(hotdogs)
 
-############# stringsAsFactors #############
+### stringsAsFactors ###
 # Import swimming_pools.csv correctly: pools
-pools <- read.csv("swimming_pools.csv", header = TRUE, sep = ",", dec = ".", stringsAsFactors = FALSE)
-
-# Check the structure of pools
+pools <- read.csv("swimming_pools.csv", header = TRUE,
+                  sep = ",", dec = ".",
+                  stringsAsFactors = FALSE)
 str(pools)
 
-############# Arguments #############
+### Arguments ###
 # Finish the read.delim() call
-hotdogs <- read.delim("hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
+hotdogs <- read.delim("hotdogs.txt", header = FALSE,
+                      col.names = c("type", "calories", "sodium"))
 
 # Select the hot dog with the least calories: lily
 lily <- hotdogs[which.min(hotdogs$calories), ]
@@ -43,97 +43,61 @@ lily <- hotdogs[which.min(hotdogs$calories), ]
 # Select the observation with the most sodium: tom
 tom <- hotdogs[which.max(hotdogs$sodium),]
 
-# Print lily and tom
-lily
-tom
-
-############# Column classes #############
+### Column classes ###
 # Previous call to import hotdogs.txt
-hotdogs <- read.delim("hotdogs.txt", header = FALSE, col.names = c("type", "calories", "sodium"))
-
-# Display structure of hotdogs
+hotdogs <- read.delim("hotdogs.txt", header = FALSE,
+                      col.names = c("type", "calories", "sodium"))
 str(hotdogs)
 
 # Edit the colClasses argument to import the data correctly: hotdogs2
-hotdogs2 <- read.delim("hotdogs.txt", header = FALSE, 
+hotdogs2 <- read.delim("hotdogs.txt", header = FALSE,
                        col.names = c("type", "calories", "sodium"),
                        colClasses = c("factor", "NULL", "numeric") )
 
+##### readr & data.table #####
 
-# Display structure of hotdogs2
-str(hotdogs2)
-
-######################################## readr & data.table ########################################
-
-############# read_delim #############
-# Just as read.table() was the main utils function, read_delim() is the main readr function.
-# Load the readr package
-library(readr)
-
+### read_delim ###
 # Import potatoes.txt using read_delim(): potatoes
 potatoes <- read_delim("potatoes.txt", delim = "\t")
-# potatoes <- read_tsv("potatoes.txt", col_types = "iiiiiddd")
 
-# Print out potatoes
-potatoes
-
-############# read_csv #############
-# readr is already loaded
-
+### read_csv ###
 # Column names
-properties <- c("area", "temp", "size", "storage", "method", 
+properties <- c("area", "temp", "size", "storage", "method",
                 "texture", "flavor", "moistness")
 
 # Import potatoes.csv with read_csv(): potatoes
 potatoes <- read_csv("potatoes.csv", col_names = properties)
 
-############# col_types, skip and n_max #############
-# readr is already loaded
-
+### col_types, skip and n_max ###
 # Column names
-properties <- c("area", "temp", "size", "storage", "method", 
+properties <- c("area", "temp", "size", "storage", "method",
                 "texture", "flavor", "moistness")
 
-# Finish the first read_tsv() call to import observations 7, 8, 9, 10 and 11 from potatoes.txt.
 # Import 5 observations from potatoes.txt: potatoes_fragment
-potatoes_fragment <- read_tsv("potatoes.txt", skip = 7, n_max = 5, col_names = properties)
+potatoes_fragment <- read_tsv("potatoes.txt", skip = 7,
+                              n_max = 5, col_names = properties)
 
 # Import all data, but force all columns to be character: potatoes_char
 potatoes_char <- read_tsv("potatoes.txt", col_types = "cccccccc")
 
-############# col_types with collectors #############
-# readr is already loaded
-
+### col_types with collectors ###
 # Import without col_types
 hotdogs <- read_tsv("hotdogs.txt", col_names = c("type", "calories", "sodium"))
-
-# Display the summary of hotdogs
-summary(hotdogs)
 
 # The collectors you will need to import the data
 fac <- col_factor(levels = c("Beef", "Meat", "Poultry"))
 int <- col_integer()
 
-# Edit the col_types argument: Pass a list() with the elements fac, int and int, so the first column is importead as a factor, and the second and third column as integers.
-# Edit the col_types argument to import the data correctly: hotdogs_factor
-hotdogs_factor <- read_tsv("hotdogs.txt", 
+hotdogs_factor <- read_tsv("hotdogs.txt",
                            col_names = c("type", "calories", "sodium"),
                            col_types = list(fac, int, int))
 
 # Display the summary of hotdogs_factor
 summary(hotdogs_factor)
 
-############# fread #############
-# load the data.table package
-library(data.table)
-
+### fread ###
 # Import potatoes.txt with fread(): potatoes
 potatoes <- fread("potatoes.txt")
-
-# Print out potatoes
-potatoes
-
-# fread is already loaded
 
 # Import columns 6 and 8 of potatoes.txt: potatoes
 potatoes <- fread("potatoes.txt", select = c(6, 8))
