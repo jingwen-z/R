a6a52011d6966084f191e816281f2c2c17900666
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-11-28 12:24:42 +0100

    reformat

--- DataCamp/Introduction-to-Machine-Learning/Classification.R
+++ DataCamp/Introduction-to-Machine-Learning/Classification.R
@@ -1,32 +1,20 @@
-##################################### Decision trees #####################################
-
-################ Learn a decision tree ################
-# The train and test set are loaded into your workspace.
-
-# Set random seed. Don't remove this line
-set.seed(1)
-
-# Load the rpart, rattle, rpart.plot and RColorBrewer package
+library(RColorBrewer)
+library(ROCR)
+library(class)
 library(rpart)
 library(rattle)
 library(rpart.plot)
-library(RColorBrewer)
 
-# Fill in the ___, build a tree model: tree
+
+########## Decision trees ##########
+
+##### Learn a decision tree #####
+set.seed(1)
+
 tree <- rpart(Survived ~ ., train, method = "class")
-# tree <- rpart(Survived ~ Pclass + Sex + Age, train, method = "class")
-
-# Draw the decision tree
 fancyRpartPlot(tree)
 
-################ Classify with the decision tree ################
-# The train and test set are loaded into your workspace.
-
-# Code from previous exercise
-set.seed(1)
-library(rpart)
-tree <- rpart(Survived ~ ., train, method = "class")
-
+##### Classify with the decision tree #####
 # Predict the values of the test set: pred
 pred <- predict(tree, test, type = "class")
 
@@ -35,15 +23,12 @@ conf <- table(test$Survived, pred)
 
 # Print out the accuracy
 sum( diag(conf) ) / sum(conf)
-# 0.7990654
-# Around 80 percent of all test instances have been classified correctly. That's not bad!
 
-################ Pruning the tree ################
-# All packages are pre-loaded, as is the data
-
-# Calculation of a complex tree
+##### Pruning the tree #####
 set.seed(1)
-tree <- rpart(Survived ~ ., train, method = "class", control = rpart.control(cp=0.00001))
+tree <- rpart(Survived ~ ., train,
+              method = "class",
+              control = rpart.control(cp=0.00001))
 
 # Draw the complex tree
 fancyRpartPlot(tree)
@@ -54,14 +39,7 @@ pruned <- prune(tree, cp = 0.01)
 # Draw pruned
 fancyRpartPlot(pruned)
 
-# Another way to check if you overfit your model is 
-# by comparing the accuracy on the training set with the accuracy on the test set.
-# You'd see that the difference between those two is smaller for the simpler tree.
-
-################ Splitting criterion ################
-# All packages, emails, train, and test have been pre-loaded
-
-# Set random seed. Don't remove this line.
+##### Splitting criterion #####
 set.seed(1)
 
 # Train and test tree with gini criterion
@@ -71,7 +49,8 @@ conf_g <- table(test$spam, pred_g)
 acc_g <- sum(diag(conf_g)) / sum(conf_g)
 
 # Change the first line of code to use information gain as splitting criterion
-tree_i <- rpart(spam ~ ., train, method = "class", parms = list(split = "information"))
+tree_i <- rpart(spam ~ ., train, method = "class",
+                parms = list(split = "information"))
 pred_i <- predict(tree_i, test, type = "class")
 conf_i <- table(test$spam, pred_i)
 acc_i <- sum(diag(conf_i)) / sum(conf_i)
@@ -81,17 +60,13 @@ fancyRpartPlot(tree_g)
 fancyRpartPlot(tree_i)
 
 # Print out acc_g and acc_i
-acc_g  # 0.8905797
-acc_i  # 0.8963768
+acc_g
+acc_i
 
-# using a different splitting criterion can influence the resulting model of your learning algorithm
 
-##################################### k-Nearest Neighbors (k-NN) #####################################
+########## k-Nearest Neighbors (k-NN) ##########
 
-################ Preprocess the data ################
-# train and test are pre-loaded
-
-# Store the Survived column of train and test in train_labels and test_labels
+##### Preprocess the data #####
 train_labels <- train$Survived
 test_labels <- test$Survived
 
@@ -100,12 +75,8 @@ knn_train <- train
 knn_test <- test
 
 # Drop Survived column for knn_train and knn_test
-# dropping a column named column in a data frame named df can be done as follows: df$column <- NULL
 knn_train$Survived <- NULL
 knn_test$Survived <- NULL
-
-# To define the minimum and maximum, only the training set is used;
-# we can't use information on the test set (like the minimums or maximums) to normalize the data.
 
 # Normalize Pclass
 min_class <- min(knn_train$Pclass)
@@ -119,128 +90,70 @@ max_age <- max(knn_train$Age)
 knn_train$Age <- (knn_train$Age - min_age) / (max_age - min_age)
 knn_test$Age <- (knn_test$Age - min_age) / (max_age - min_age)
 
-# the variable Sex already took values between 0 and 1, hence did not require normalization
 
 ################ The knn() function ################
-# knn_train, knn_test, train_labels and test_labels are pre-loaded
-
-# Set random seed. Don't remove this line.
 set.seed(1)
 
-# Load the class package
-library(class)
-
-# Fill in the ___, make predictions using knn: pred
 pred <- knn(train = knn_train, test = knn_test, cl = train_labels, k = 5)
-    
-# Construct the confusion matrix: conf
-conf <- table(test_labels, pred)
 
-# Print out the confusion matrix
+conf <- table(test_labels, pred)
 conf
 
-################ K's choice ################
-# knn_train, knn_test, train_labels and test_labels are pre-loaded
-
-# Set random seed. Don't remove this line.
+##### K's choice #####
 set.seed(1)
 
-# Load the class package, define range and accs
-library(class)
 range <- 1:round(0.2 * nrow(knn_train))
 accs <- rep(0, length(range))
 
 for (k in range) {
-  
-  # Fill in the ___, make predictions using knn: pred
   pred <- knn(train = knn_train, test = knn_test, cl = train_labels, k = k)
-    
-  # Fill in the ___, construct the confusion matrix: conf
   conf <- table(test_labels, pred)
-    
-  # Fill in the ___, calculate the accuracy and store it in accs[k]
-  accs[k] <- sum( diag(conf) ) / sum(conf)
+  accs[k] <- sum(diag(conf)) / sum(conf)
 }
 
-# Plot the accuracies. Title of x-axis is "k".
 plot(range, accs, xlab = "k")
 
-# Calculate the best k
-# find out which index is highest in the accs vector
 which.max(accs)
-# the best k is 73
 
-##################################### The ROC curve #####################################
 
-################ Creating the ROC curve ################
-# train and test are pre-loaded
+########## The ROC curve ##########
 
-# Set random seed. Don't remove this line
+##### Creating the ROC curve #####
 set.seed(1)
 
-# Build a tree on the training set: tree
 tree <- rpart(income ~ ., train, method = "class")
 
-# Predict probability values using the model: all_probs
 all_probs <- predict(tree, test, type = "prob")
-
-# Print out all_probs
 all_probs
 
-# Select second column of all_probs: probs
 probs <- all_probs[ , 2]
-
-# Load the ROCR library
-library(ROCR)
 
 # Make a prediction object: pred
 pred <- prediction(probs, test$income)
 
 # Make a performance object: perf
 perf <- performance(pred, "tpr", "fpr")
-
-# Plot this curve
 plot(perf)
 
-################ The area under the curve (AUC) ################
-# test and train are loaded into your workspace
-
-# Build tree and predict probability values for the test set
+##### The area under the curve (AUC) #####
 set.seed(1)
 tree <- rpart(income ~ ., train, method = "class")
 probs <- predict(tree, test, type = "prob")[,2]
 
-# Load the ROCR library
-library(ROCR)
-
-# Make a prediction object: pred
 pred <- prediction(probs, test$income)
-
-# Make a performance object: perf
 perf <- performance(pred, "auc")
 
-# Print out the AUC
 AUC <- perf@y.values[[1]]
-AUC  # 0.8463732
+AUC
 
-################ Comparing the methods (a decision tree model VS a k-Nearest Neighbor model) ################
-# Load the ROCR library
-library(ROCR)
+############# A decision tree model VS a k-Nearest Neighbor model #############
 
-# The assigned probabilities for the observations in the test set are loaded into the workspace: 
-# probs_t for the decision tree model, probs_k for k-Nearest Neighbors.
-
-# Make the prediction objects for both models: pred_t, pred_k
 pred_t <- prediction(probs_t, test$spam)
 pred_k <- prediction(probs_k, test$spam)
 
 # Make the performance objects for both models: perf_t, perf_k
 perf_t <- performance(pred_t, "tpr", "fpr")
 perf_k <- performance(pred_k, "tpr", "fpr")
-
-# A predefined functions has been defined for you: draw_roc_lines(). 
-# It takes two arguments: the first is the performance object of the tree model, perf_t, 
-# and the second is the performance object of the k-Nearest Neighbor model, perf_k.
 
 # Draw the ROC lines using draw_roc_lines()
 draw_roc_lines(perf_t, perf_k)
