d985f1f0c13202589aaeeb99490be0f83dfe7efb
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-12-06 18:30:56 +0100

    reformat

--- DataCamp/Importing-Data-Into-R/Importing data from Excel.R
+++ DataCamp/Importing-Data-Into-R/Importing data from Excel.R
@@ -1,10 +1,10 @@
+library(gdata)
 library(readxl)
 library(XLConnect)
 
 ###### The readxl package ######
 
 ### List the sheets of an Excel file ###
-# Print out the names of both spreadsheets
 excel_sheets("latitude.xlsx")
 
 ### Import an Excel sheet ###
@@ -16,15 +16,13 @@ latitude_2 <- read_excel("latitude.xlsx", sheet = "1900")
 
 # Put latitude_1 and latitude_2 in a list: lat_list
 lat_list <- list(latitude_1, latitude_2)
-
-# Display the structure of lat_list
 str(lat_list)
 
 ### Reading a workbook ###
 # Read all Excel sheets with lapply(): lat_list
-lat_list <- lapply(excel_sheets("latitude.xlsx"), read_excel, path = "latitude.xlsx")
-
-# Display the structure of lat_list
+lat_list <- lapply(excel_sheets("latitude.xlsx"),
+                   read_excel,
+                   path = "latitude.xlsx")
 str(lat_list)
 
 ### The col_names argument ###
@@ -38,17 +36,13 @@ summary(latitude_4)
 
 ### The skip argument ###
 # Import the second sheet of latitude.xlsx, skipping the first 21 rows: latitude_sel
-latitude_sel <- read_excel("latitude.xlsx", sheet = 2, col_names = FALSE, skip = 21)
+latitude_sel <- read_excel("latitude.xlsx", sheet = 2, col_names = F, skip = 21)
 
-# Print out the first observation from latitude_sel
 latitude_sel[1, ]
 
 ###### The gdata package ######
 
 ### Import a local file ###
-# Load the gdata package
-library(gdata)
-
 # Import the second sheet of urbanpop.xls: urban_pop
 urban_pop <- read.xls("urbanpop.xls", sheet = "1967-1974")
 
@@ -59,26 +53,26 @@ head(urban_pop, 11)
 columns <- c("country", paste0("year_", 1967:1974))
 
 # Finish the read.xls call: skip the first 50 rows of the sheet.
-urban_pop <- read.xls("urbanpop.xls", sheet = 2,
-                      skip = 50, header = FALSE,
-                      stringsAsFactors = FALSE,
+urban_pop <- read.xls("urbanpop.xls",
+                      sheet = 2,
+                      skip = 50,
+                      header = F,
+                      stringsAsFactors = F,
                       col.names = columns)
 head(urban_pop, 10)
 
 ### Work that Excel data! ###
 # Add code to import data from all three sheets in urbanpop.xls
 path <- "urbanpop.xls"
-urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = FALSE)
-urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = FALSE)
-urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = FALSE)
+urban_sheet1 <- read.xls(path, sheet = 1, stringsAsFactors = F)
+urban_sheet2 <- read.xls(path, sheet = 2, stringsAsFactors = F)
+urban_sheet3 <- read.xls(path, sheet = 3, stringsAsFactors = F)
 
 # Extend the cbind() call to include urban_sheet3: urban
 urban <- cbind(urban_sheet1, urban_sheet2[-1], urban_sheet3[-1])
 
 # Remove all rows with NAs from urban: urban_clean
 urban_clean <- na.omit(urban)
-
-# Print out a summary of urban_clean
 summary(urban_clean)
 
 ###### The XLConnect package ######
