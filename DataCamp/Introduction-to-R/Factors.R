1ce32ed8bca893a37273ad7876e07e556bd6c723
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-12-16 18:30:56 +0100

    reformat

--- DataCamp/Introduction-to-R/Factors.R
+++ DataCamp/Introduction-to-R/Factors.R
@@ -1,4 +1,4 @@
-############# What's a factor and why would you use it? #############
+##### What's a factor and why would you use it? #####
 # Assign to the variable theory what this chapter is about!
 theory <- 'factors for categorical variables'
 
@@ -7,9 +7,6 @@ gender_vector <- c("Male", "Female", "Female", "Male", "Male")
 
 # Convert gender_vector to a factor
 factor_gender_vector <- factor(gender_vector)
-  
-# Print out factor_gender_vector
-factor_gender_vector
 
 # Animals
 animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
@@ -20,12 +17,8 @@ factor_animals_vector
 temperature_vector <- c("High", "Low", "High","Low", "Medium")
 factor_temperature_vector <- factor(temperature_vector, order = TRUE,
                                     levels = c("Low", "Medium", "High"))
-factor_temperature_vector
 
-############# Factor levels #############
-
-## levels(factor_vector) <- c("name1", "name2",...)
-
+##### Factor levels #####
 # Code to build factor_survey_vector
 survey_vector <- c("M", "F", "F", "M", "M")
 factor_survey_vector <- factor(survey_vector)
@@ -34,27 +27,24 @@ factor_survey_vector <- factor(survey_vector)
 levels(factor_survey_vector) <- c("Female", "Male")
 factor_survey_vector
 
-############# Summarizing a factor #############
+##### Summarizing a factor #####
 # Build factor_survey_vector with clean levels
 survey_vector <- c("M", "F", "F", "M", "M")
 factor_survey_vector <- factor(survey_vector)
 levels(factor_survey_vector) <- c("Female", "Male")
 factor_survey_vector
 
-# Generate summary for survey_vector
 summary(survey_vector)
-
-# Generate summary for factor_survey_vector
 summary(factor_survey_vector)
 
-############# Battle of the sexes #############
+##### Battle of the sexes #####
 # Build factor_survey_vector with clean levels
 survey_vector <- c( "M", "F", "F", "M", "M" )
 factor_survey_vector <- factor(survey_vector)
 levels(factor_survey_vector) <- c("Female", "Male")
 
 # Male
-male <- factor_survey_vector[1] 
+male <- factor_survey_vector[1]
 
 # Female
 female <- factor_survey_vector[2]
@@ -62,7 +52,7 @@ female <- factor_survey_vector[2]
 # Battle of the sexes: Male 'larger' than female?
 male > female
 
-############# Ordered factors #############
+##### Ordered factors #####
 # Create speed_vector
 speed_vector <- c("fast", "slow", "slow", "fast", "insane")
 
@@ -72,9 +62,9 @@ factor_speed_vector <- factor(speed_vector, ordered = TRUE,
 
 # Print factor_speed_vector
 factor_speed_vector
-summary(factor_speed_vector) 
+summary(factor_speed_vector)
 
-############# Comparing ordered factors #############
+##### Comparing ordered factors #####
 # Create factor_speed_vector
 speed_vector <- c("fast", "slow", "slow", "fast", "insane")
 factor_speed_vector <- factor(speed_vector, ordered = TRUE,
