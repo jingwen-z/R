3d5d8d48976bd377d80755f62ae708c52f9832b8
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-12-17 18:30:56 +0100

    reformat

--- DataCamp/Introduction-to-R/Intro to basics.R
+++ DataCamp/Introduction-to-R/Intro to basics.R
@@ -1,22 +1,22 @@
-############## How it works ##############
+##### How it works #####
 # Calculate 3 + 4
 3 + 4
 
 # Calculate 6 + 12
 6 + 12
 
-############## Arithmetic with R ##############
+##### Arithmetic with R #####
 # An addition
-5 + 5 
+5 + 5
 
 # A subtraction
-5 - 5 
+5 - 5
 
 # A multiplication
 3 * 5
 
  # A division
-(5 + 5) / 2 
+(5 + 5) / 2
 
 # Exponentiation
 2 ^ 5
@@ -24,7 +24,7 @@
 # Modulo
 28 %% 6
 
-############## Variable assignment ##############
+##### Variable assignment #####
 # Assign the value 42 to x
 x <- 42
 
@@ -47,18 +47,18 @@ my_apples + my_oranges
 # Create the variable my_fruit
 my_fruit <- my_apples + my_oranges
 
-############## Apples and oranges ##############
+##### Apples and oranges #####
 # Assign a value to the variable my_apples
-my_apples <- 5 
+my_apples <- 5
 
 # Fix the assignment of my_oranges
 my_oranges <- 6
 
 # Create the variable my_fruit and print it out
-my_fruit <- my_apples + my_oranges 
+my_fruit <- my_apples + my_oranges
 my_fruit
-  
-############## Basic data types in R ##############
+
+##### Basic data types in R #####
 # Change my_numeric to be 42
 my_numeric <- 42
 
@@ -68,11 +68,11 @@ my_character <- "universe"
 # Change my_logical to be FALSE
 my_logical <- FALSE
 
-############## What's that data type? ##############
+##### What's that data type? #####
 # Declare variables of different types
 my_numeric <- 42
 my_character <- "universe"
-my_logical <- FALSE 
+my_logical <- FALSE
 
 # Check class of my_numeric
 class(my_numeric)
