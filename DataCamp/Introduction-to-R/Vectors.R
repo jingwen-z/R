47ebd0e4bfb12849dfb5366f4616f01f539ad69e
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-12-21 18:30:56 +0100

    reformat

--- DataCamp/Introduction-to-R/Vectors.R
+++ DataCamp/Introduction-to-R/Vectors.R
@@ -1,4 +1,4 @@
-############# Create a vector #############
+### Create a vector ###
 # Define the variable vegas
 vegas <- "Go!"
 
@@ -6,15 +6,15 @@ numeric_vector <- c(1, 10, 49)
 character_vector <- c("a", "b", "c")
 
 # Complete the code for boolean_vector
-boolean_vector <- c(TRUE,FALSE,TRUE)
+boolean_vector <- c(TRUE, FALSE, TRUE)
 
 # Poker winnings from Monday to Friday
 poker_vector <- c(140, -50, 20, -120, 240)
 
 # Roulette winnings from Monday to Friday
-roulette_vector <- c(-24, -50, 100, -350, 10) 
+roulette_vector <- c(-24, -50, 100, -350, 10)
 
-############# Naming a vector #############
+### Naming a vector ###
 # Poker winnings from Monday to Friday
 poker_vector <- c(140, -50, 20, -120, 240)
 
@@ -25,7 +25,8 @@ roulette_vector <- c(-24, -50, 100, -350, 10)
 names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
 # Assign days as names of roulette_vectors
-names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday",
+                            "Thursday", "Friday")
 
 # Poker winnings from Monday to Friday
 poker_vector <- c(140, -50, 20, -120, 240)
@@ -35,18 +36,18 @@ roulette_vector <- c(-24, -50, 100, -350, 10)
 
 # The variable days_vector
 days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
- 
+
 # Assign the names of the day to roulette_vector and poker_vector
-names(poker_vector) <- days_vector  
+names(poker_vector) <- days_vector
 names(roulette_vector) <- days_vector
 
-############# Calculating total winnings #############
+### Calculating total winnings ###
 A_vector <- c(1, 2, 3)
 B_vector <- c(4, 5, 6)
 
 # Take the sum of A_vector and B_vector
 total_vector <- A_vector + B_vector
-  
+
 # Print out total_vector
 total_vector
 
@@ -71,7 +72,7 @@ names(roulette_vector) <- days_vector
 total_poker <- sum(poker_vector)
 
 # Total winnings with roulette
-total_roulette <- sum(roulette_vector) 
+total_roulette <- sum(roulette_vector)
 
 # Total winnings overall
 total_week <- total_poker + total_roulette
@@ -79,7 +80,7 @@ total_week <- total_poker + total_roulette
 # Print out total_week
 total_week
 
-############# Comparing total winnings #############
+### Comparing total winnings ###
 # Poker and roulette winnings from Monday to Friday:
 poker_vector <- c(140, -50, 20, -120, 240)
 roulette_vector <- c(-24, -50, 100, -350, 10)
@@ -91,10 +92,10 @@ names(roulette_vector) <- days_vector
 total_poker <- sum(poker_vector)
 total_roulette <- sum(roulette_vector)
 
-# Check if you realized higher total gains in poker than in roulette 
+# Check if you realized higher total gains in poker than in roulette
 total_poker > total_roulette
 
-############# Vector selection: the good times #############
+### Vector selection: the good times ###
 # Poker and roulette winnings from Monday to Friday:
 poker_vector <- c(140, -50, 20, -120, 240)
 roulette_vector <- c(-24, -50, 100, -350, 10)
@@ -106,18 +107,18 @@ names(roulette_vector) <- days_vector
 poker_wednesday <- poker_vector[3]
 
 # Define a new variable based on a selection
-poker_midweek <- poker_vector[c(2,3,4)]
+poker_midweek <- poker_vector[c(2, 3, 4)]
 
 # Define a new variable based on a selection
 roulette_selection_vector <- roulette_vector[2:5]
 
 # Select poker results for Monday, Tuesday and Wednesday
 poker_start <- poker_vector[c("Monday","Tuesday","Wednesday")]
-  
+
 # Calculate the average of the elements in poker_start
 mean(poker_start)
 
-############# Selection by comparison #############
+### Selection by comparison ###
 # Poker and roulette winnings from Monday to Friday:
 poker_vector <- c(140, -50, 20, -120, 240)
 roulette_vector <- c(-24, -50, 100, -350, 10)
@@ -127,14 +128,12 @@ names(roulette_vector) <- days_vector
 
 # Which days did you make money on poker?
 selection_vector <- poker_vector > 0
-  
-# Print out selection_vector
 selection_vector
 
 # Select from poker_vector these days
 poker_winning_days <- poker_vector[selection_vector]
 
-############# Advanced selection #############
+### Advanced selection ###
 # Which days did you make money on roulette?
 selection_vector <- roulette_vector > 0
 
